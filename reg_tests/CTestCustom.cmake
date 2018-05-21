e0d624aba40071e1a880f0ad133e51dad1897744
Jon Rood <jon.rood@nrel.gov>
2018-05-21 14:29:25 -0600

    Forcing read of ctest custom files during testing.

--- reg_tests/CTestCustom.cmake
+++ reg_tests/CTestCustom.cmake
@@ -1,3 +1,1 @@
-SET (CTEST_CUSTOM_WARNING_EXCEPTION
-    ${CTEST_CUSTOM_WARNING_EXCEPTION}
-    ".*: warning: could not create compact unwind*")
+SET(CTEST_CUSTOM_WARNING_EXCEPTION ".*: warning: could not create compact unwind*")
