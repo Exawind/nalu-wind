b96bf7950f73f82bcf176eb57748a61860faa5ef
Robert C. Knaus <rcknaus@sandia.gov>
2018-07-17 15:03:18 -0600

    Modify scalar advection and pressure poisson mms unit tests to use variable properties

--- unit_tests/ho_kernels/UnitTestPressurePoissonHO.C
+++ unit_tests/ho_kernels/UnitTestPressurePoissonHO.C
@@ -2,8 +2,13 @@
 #include <limits>
 
 #include <kernel/TensorProductCVFEMPressurePoisson.h>
+#include <kernel/TensorProductCVFEMSource.h>
 #include <master_element/TensorProductCVFEMDiffusionMetric.h>
 #include <master_element/TensorProductCVFEMAdvectionMetric.h>
+#include <master_element/TensorProductCVFEMVolumeMetric.h>
+
+
+
 #include <master_element/CVFEMCoefficientMatrices.h>
 #include <Kokkos_Core.hpp>
 #include <Teuchos_BLAS.hpp>
@@ -18,56 +23,58 @@
 #include "UnitTestViewUtils.h"
 #include "UnitTestUtils.h"
 
+
+namespace sierra{
+namespace nalu {
 namespace {
 
-
-  //--------------------------------------------------------------
-  template<typename T>
-  T square_transpose(const T A)
-  {
-    T At(A.label()+"_t");
-
-    EXPECT_EQ(A.extent(0), A.extent(1));
-    const int n = A.extent_int(0);
-    for (int j = 0; j < n; ++j) {
-      for (int i = 0; i < n; ++i) {
-        At(i,j) = A(j,i);
-      }
-    }
-    return At;
-  }
-
   struct TestFunction {
-    double val(int d, double x, double y, double z) const
+    double uval(double x, double y, double z, int d) const
     {
-      const double zcoeff = (c[2] != 0 ) ? -(c[0] + c[1])/c[2] : 0;
+      const double zcoeff = (kz != 0) ? -(kx + ky) / kz : 0;
       switch (d)
       {
         case 0:
-         return std::cos(c[0] * x) * std::sin(c[1] * y) * std::sin(c[2] * z);
+          return std::cos(kx * x) * std::sin(ky * y) * std::sin(kz * z);
         case 1:
-          return std::sin(c[0] * x) * std::cos(c[1] * y) * std::sin(c[2] * z);
+          return std::sin(kx * x) * std::cos(ky * y) * std::sin(kz * z);
         case 2:
-          return zcoeff * std::sin(c[0] * x) * std::sin(c[1] * y) * std::cos(c[2] * z);
+          return zcoeff * std::sin(kx * x) * std::sin(ky * y) * std::cos(kz * z);
         default:
           return 0;
       }
     }
-    std::array<double, 3> c{{1*M_PI,1*M_PI,1*M_PI}};
+
+    double rho(double x, double y, double z) const
+    {
+      return (2 + std::sin(kx * x) * std::sin(ky * y) * std::sin(2 * kz * z));
+    }
+
+    double residual(double x, double y, double z) const
+    {
+      return -2*(kx + ky)*std::cos(kz*z)*std::cos(2*kz*z)*std::pow(std::sin(kx*x),2)*std::pow(std::sin(ky*y),2)
+      + (ky*std::pow(std::cos(ky*y),2)*std::pow(std::sin(kx*x),2) + kx*std::pow(std::cos(kx*x),2)*std::pow(std::sin(ky*y),2))*std::sin(kz*z)*std::sin(2*kz*z);
+    }
+
+    const double kx{M_PI};
+    const double ky{M_PI};
+    const double kz{M_PI};
   };
 
   template <int p> void mms()
   {
     TestFunction test;
 
-    sierra::nalu::nodal_vector_workview<p, double> l_velocity; auto& velocity = l_velocity.view();
-    sierra::nalu::nodal_scalar_workview<p, double> l_pressure; auto& pressure = l_pressure.view();
-    sierra::nalu::nodal_scalar_workview<p, double> l_density; auto& density = l_pressure.view();
-    sierra::nalu::nodal_vector_workview<p, double> l_Gp; auto& Gp = l_Gp.view();
+    nodal_vector_workview<p, double> work_velocity; auto& velocity = work_velocity.view();
+    nodal_scalar_workview<p, double> work_density; auto& density = work_density.view();
 
-    typename sierra::nalu::nodal_vector_workview<p, double> l_coords; auto& coords = l_coords.view();
+    nodal_scalar_workview<p, double> work_pressure(1); auto& pressure = work_pressure.view();
+    nodal_vector_workview<p, double> work_Gp(0); auto& Gp = work_Gp.view();
 
-    std::vector<double> coords1D = sierra::nalu::gauss_lobatto_legendre_rule(p+1).first;
+    nodal_vector_workview<p, double> work_coords; auto& coords = work_coords.view();
+    nodal_scalar_workview<p, double> work_exact_resid; auto& exact_resid = work_exact_resid.view();
+
+    std::vector<double> coords1D = gauss_lobatto_legendre_rule(p+1).first;
     for (int k = 0; k < p + 1; ++k) {
       const double z = coords1D[k];
       for (int j = 0; j < p+1; ++j) {
@@ -77,41 +84,46 @@ namespace {
           coords(k, j, i, 0) = x;
           coords(k, j, i, 1) = y;
           coords(k, j, i, 2) = z;
-          Gp(k, j, i, 0) = 0;
-          Gp(k, j, i, 1) = 0;
-          Gp(k, j, i, 2) = 0;
 
-          velocity(k, j, i, 0) = test.val(0, x, y, z);
-          velocity(k, j, i, 1) = test.val(1, x, y, z);
-          velocity(k, j, i, 2) = test.val(2, x, y, z);
 
-          pressure(k, j, i) = 0;
-          density(k, j, i) = 1;
+          velocity(k, j, i, 0) = test.uval(x, y, z, XH);
+          velocity(k, j, i, 1) = test.uval(x, y, z, YH);
+          velocity(k, j, i, 2) = test.uval(x, y, z, ZH);
 
+          density(k, j, i) = test.rho(x,y,z);
+
+          exact_resid(k, j, i) = test.residual(x, y, z);
         }
       }
     }
 
-    auto ops = sierra::nalu::CVFEMOperators<p, double>();
+    auto ops = CVFEMOperators<p, double>();
 
-    sierra::nalu::scs_vector_workview<p, double> l_metric(0);
-    sierra::nalu::high_order_metrics::compute_laplacian_metric_linear(ops, coords, l_metric.view());
+    nodal_scalar_workview<p, double> work_vol; auto& vol = work_vol.view();
+    high_order_metrics::compute_volume_metric_linear(ops, coords, vol);
 
-    sierra::nalu::scs_scalar_workview<p, double> l_mdot(0);
-    sierra::nalu::high_order_metrics::compute_mdot_linear(ops, coords, l_metric.view(), 1.0, density, velocity, Gp, pressure, l_mdot.view());
+    nodal_scalar_workview<p, double> work_rhs(0); auto& rhs = work_rhs.view();
 
-    sierra::nalu::nodal_scalar_workview<p, double> l_rhs(0);
-    auto& rhs = l_rhs.view();
-    sierra::nalu::tensor_assembly::pressure_poisson_rhs(ops, 1.0, l_mdot.view(), rhs);
+    // compute source from exact residual:
+    tensor_assembly::add_volumetric_source(ops, vol, exact_resid, rhs);
+
+    scs_vector_workview<p, double> work_metric(0);
+    high_order_metrics::compute_laplacian_metric_linear(ops, coords, work_metric.view());
+
+    scs_scalar_workview<p, double> work_mdot(0);
+    high_order_metrics::compute_mdot_linear(ops, coords, work_metric.view(), 1.0, density, velocity, Gp, pressure, work_mdot.view());
+
+    tensor_assembly::pressure_poisson_rhs(ops, 1.0, work_mdot.view(), rhs);
 
     for (int k = 1; k < p; ++k) {
       for (int j = 1; j < p; ++j) {
         for (int i = 1; i < p; ++i) {
-         ASSERT_NEAR(rhs(k,j,i), 0, 1.0e-8) << "(k,j,i) = (" << k << ", " << j << ", " << i << ")";
+          EXPECT_NEAR(rhs(k,j,i), 0, 1.0e-8) << "(k,j,i) = (" << k << ", " << j << ", " << i << ")";
         }
       }
     }
   }
 }
 //--------------------------------------------------------------
-TEST_POLY(PressurePoissonHex, mms, 20);
+TEST_POLY(PressurePoissonHex, mms, 22);
+}}
--- unit_tests/ho_kernels/UnitTestScalarAdvDiffHO.C
+++ unit_tests/ho_kernels/UnitTestScalarAdvDiffHO.C
@@ -25,11 +25,6 @@ namespace {
 
 struct TestFunction
 {
-  double val(double x, double y, double z) const
-  {
-    return std::cos(2 * kx * x) * std::cos(ky * y) * std::cos(kz * z);
-  }
-
   double uval(double x, double y, double z, int d) const
   {
     const double zcoeff = (kz != 0) ? -(kx + ky) / kz : 0;
@@ -46,14 +41,27 @@ struct TestFunction
     }
   }
 
-  double laplacian(double x, double y, double z) const
+  double val(double x, double y, double z) const
   {
-    return -(4 * kx * kx + ky * ky + kz * kz) * val(x, y, z);
+    return std::cos(2 * kx * x) * std::cos(ky * y) * std::cos(kz * z);
   }
 
-  double advection(double x, double y, double z) const
+  double gamma(double x, double y, double z) const {
+    return (2 + std::cos(kx * x) * std::cos(2 * ky * y) * std::cos(kz * z));
+  }
+
+  double rho(double x, double y, double z) const
   {
-    return -(kx * (3 * std::sin(kx * x) + std::sin(3 * kx * x)) * std::sin(2 * ky * y) * std::sin(2 * kz * z)) / 8.;
+    return (2 + std::sin(kx * x) * std::sin(ky * y) * std::sin(2 * kz * z));
+  }
+
+  double residual(double x, double y, double z) const {
+    return -(std::cos(2*kx*x)*(2*std::cos(ky*y)*(-2*(4*std::pow(kx,2) + std::pow(ky,2) + std::pow(kz,2))*std::cos(kz*z) +
+        std::pow(std::cos(kz*z),2)*(std::cos(kx*x)*(2*std::pow(ky,2) - (4*std::pow(kx,2) + 3*std::pow(ky,2) + std::pow(kz,2))*std::cos(2*ky*y)) +
+           2*(kx + ky)*std::cos(2*kz*z)*std::pow(std::sin(kx*x),2)*std::pow(std::sin(ky*y),2)) + std::pow(kz,2)*std::cos(kx*x)*std::cos(2*ky*y)*std::pow(std::sin(kz*z),2)) -
+     kx*std::sin(kx*x)*std::sin(2*ky*y)*std::sin(2*kz*z) - std::cos(ky*y)*(ky*std::pow(std::cos(ky*y),2)*std::pow(std::sin(kx*x),2) + kx*std::pow(std::sin(ky*y),2))*std::pow(std::sin(2*kz*z),2)))/2. -
+kx*std::sin(kx*x)*std::sin(2*kx*x)*(1.0/std::tan(kx*x)*std::sin(2*ky*y)*std::sin(2*kz*z) +
+  std::cos(ky*y)*(2*kx*std::cos(2*ky*y)*std::pow(std::cos(kz*z),2) + std::cos(kx*x)*std::pow(std::sin(ky*y),2)*std::pow(std::sin(2*kz*z),2)));
   }
 
   const double kx{ M_PI };
@@ -61,24 +69,23 @@ struct TestFunction
   const double kz{ M_PI };
 };
 
-
 template <int p> void mms()
 {
-  // check that we converge given a single, very high p element
-  sierra::nalu::nodal_vector_workview<p, double> work_coords; auto& coords = work_coords.view();
-  sierra::nalu::nodal_vector_workview<p, double> work_velocity; auto& velocity = work_velocity.view();
-  sierra::nalu::nodal_scalar_workview<p, double> work_scalar; auto& scalar = work_scalar.view();
+  // check that we get the correct answer given a single, very high p element
+  nodal_vector_workview<p, double> work_coords; auto& coords = work_coords.view();
+  nodal_vector_workview<p, double> work_velocity; auto& velocity = work_velocity.view();
+  nodal_scalar_workview<p, double> work_scalar; auto& scalar = work_scalar.view();
 
   // constants
-  sierra::nalu::nodal_scalar_workview<p, double> work_diffusivity(1); auto& diffusivity = work_diffusivity.view();
-  sierra::nalu::nodal_scalar_workview<p, double> work_rho(1); auto& rho = work_rho.view();
-  sierra::nalu::nodal_vector_workview<p, double> work_Gp(0); auto& Gp = work_Gp.view();
-  sierra::nalu::nodal_scalar_workview<p, double> work_pressure(1); auto& pressure = work_pressure.view();
+  nodal_scalar_workview<p, double> work_diffusivity(1); auto& diffusivity = work_diffusivity.view();
+  nodal_scalar_workview<p, double> work_rho(1); auto& rho = work_rho.view();
+  nodal_vector_workview<p, double> work_Gp(0); auto& Gp = work_Gp.view();
+  nodal_scalar_workview<p, double> work_pressure(1); auto& pressure = work_pressure.view();
 
   TestFunction test;
-  sierra::nalu::nodal_scalar_workview<p, double> work_exact_resid(0); auto& exact_resid = work_exact_resid.view();
+  nodal_scalar_workview<p, double> work_exact_resid(0); auto& exact_resid = work_exact_resid.view();
 
-  std::vector<double> coords1D = sierra::nalu::gauss_lobatto_legendre_rule(p+1).first;
+  std::vector<double> coords1D = gauss_lobatto_legendre_rule(p+1).first;
   for (int k = 0; k < p + 1; ++k) {
     const double z = coords1D[k];
     for (int j = 0; j < p+1; ++j) {
@@ -89,37 +96,40 @@ template <int p> void mms()
         coords(k, j, i, YH) = y;
         coords(k, j, i, ZH) = z;
 
+        rho(k, j, i) = test.rho(x, y, z);
+        diffusivity(k, j, i) = test.gamma(x, y, z);
+
         velocity(k, j, i, XH) = test.uval(x, y, z, XH);
         velocity(k, j, i, YH) = test.uval(x, y, z, YH);
         velocity(k, j, i, ZH) = test.uval(x, y, z, ZH);
 
         scalar(k,j,i) = test.val(x,y,z);
 
-        exact_resid(k, j, i) = -test.laplacian(x, y, z) + test.advection(x,y,z);
+        exact_resid(k, j, i) = test.residual(x,y,z);
       }
     }
   }
 
   // volume integrate source
-  sierra::nalu::CVFEMOperators<p, double> ops;
+  CVFEMOperators<p, double> ops;
 
-  sierra::nalu::nodal_scalar_workview<p, double> work_vol; auto& vol = work_vol.view();
-  sierra::nalu::high_order_metrics::compute_volume_metric_linear(ops, coords, vol);
+  nodal_scalar_workview<p, double> work_vol; auto& vol = work_vol.view();
+  high_order_metrics::compute_volume_metric_linear(ops, coords, vol);
 
-  sierra::nalu::nodal_scalar_workview<p, double> work_rhs(0); auto& rhs = work_rhs.view();
+  nodal_scalar_workview<p, double> work_rhs(0); auto& rhs = work_rhs.view();
 
   // compute source from exact residual:
-  sierra::nalu::tensor_assembly::add_volumetric_source(ops, vol, exact_resid, rhs);
+  tensor_assembly::add_volumetric_source(ops, vol, exact_resid, rhs);
 
   // compute numerical residual:
-  sierra::nalu::scs_vector_workview<p, double> work_diffmetric; auto& diffmetric = work_diffmetric.view();
-  sierra::nalu::high_order_metrics::compute_laplacian_metric_linear(ops, coords, diffmetric);
+  scs_vector_workview<p, double> work_diffmetric; auto& diffmetric = work_diffmetric.view();
+  high_order_metrics::compute_laplacian_metric_linear(ops, coords, diffmetric);
 
-  sierra::nalu::scs_scalar_workview<p, double> l_mdot(0); auto& mdot = l_mdot.view();
-  sierra::nalu::high_order_metrics::compute_mdot_linear(ops, coords, diffmetric, 1,  rho, velocity, Gp, pressure, mdot);
+  scs_scalar_workview<p, double> l_mdot(0); auto& mdot = l_mdot.view();
+  high_order_metrics::compute_mdot_linear(ops, coords, diffmetric, 1,  rho, velocity, Gp, pressure, mdot);
 
-  sierra::nalu::high_order_metrics::scale_metric(ops, diffusivity, diffmetric);
-  sierra::nalu::tensor_assembly::scalar_advdiff_rhs(ops, mdot, diffmetric, scalar, rhs);
+  high_order_metrics::scale_metric(ops, diffusivity, diffmetric);
+  tensor_assembly::scalar_advdiff_rhs(ops, mdot, diffmetric, scalar, rhs);
 
   // consider just element interior
   for (int k = 1; k < p; ++k) {
@@ -134,5 +144,5 @@ template <int p> void mms()
 
 }
 //--------------------------------------------------------------
-TEST_POLY(ScalarAdvDiff, mms, 20);
+TEST_POLY(ScalarAdvDiff, mms, 22);
 }}
