1c170cafc6c3d196b706ce275692ee68c720cbbf
Alan Williams <william@sandia.gov>
2018-07-18 09:27:54 -0600

    Fix up unit-test for alignment of shared-mem-views.
    This unit-test currently has 8 as the 'gold value'. Once a fix
    is made in Kokkos, I expect the gold value to change to
    be KOKKOS_MEMORY_ALIGNMENT.

--- unit_tests/UnitTestShmemAlignment.C
+++ unit_tests/UnitTestShmemAlignment.C
@@ -11,16 +11,25 @@ TEST(Shmem, align)
   unsigned bytes_per_thread = 2048;
 
   auto team_exec = sierra::nalu::get_device_team_policy(N, bytes_per_team, bytes_per_thread);
-  Kokkos::parallel_for(team_exec, KOKKOS_LAMBDA(const sierra::nalu::TeamHandleType& team)
+  unsigned numCorrectlyAlignedViews = 0;
+  Kokkos::parallel_reduce(team_exec, KOKKOS_LAMBDA(const sierra::nalu::TeamHandleType& team, unsigned& localResult)
   {
     sierra::nalu::SharedMemView<double*> view1 = sierra::nalu::get_shmem_view_1D<double>(team, numScalars);
-    std::cerr<<"view1.data(): "<<view1.data()<<", alignof(view1.data()): "<<alignof(view1.data())<<std::endl;
+    if (alignof(view1.data()) == 8) {
+        ++localResult;
+    }
 
     sierra::nalu::SharedMemView<double*> view2 = sierra::nalu::get_shmem_view_1D<double>(team, numScalars);
-    std::cerr<<"view2-view1: "<<ptrdiff_t(view2.data()-view1.data())*sizeof(double)<<", view2.data(): "<<view2.data()<<", alignof(view2.data()): "<<alignof(view2.data())<<std::endl;
+    if (alignof(view1.data()) == 8) {
+        ++localResult;
+    }
 
     sierra::nalu::SharedMemView<double*> view3 = sierra::nalu::get_shmem_view_1D<double>(team, numScalars);
-    std::cerr<<"view3-view2: "<<ptrdiff_t(view3.data()-view2.data())*sizeof(double)<<", view3.data(): "<<view3.data()<<", alignof(view3.data()): "<<alignof(view3.data())<<std::endl;
-  });
+    if (alignof(view1.data()) == 8) {
+        ++localResult;
+    }
+  }, numCorrectlyAlignedViews);
+
+  EXPECT_EQ(3u, numCorrectlyAlignedViews);
 }
 
