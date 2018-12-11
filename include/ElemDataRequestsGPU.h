dd20d4c78fac4926ebdb41bbcf1817c554a1e7a9
Shreyas Ananthan <shreyas@umd.edu>
2018-12-11 12:54:07 -0700

    ScratchViews prototype for execution on GPUs (#63)
    This pull request implements a unit test that demonstrates the use of ngp::Mesh, ngp::Field in constructing and executing a TestKernel with ScratchViews on GPU. It introduces a new class ElemDataRequestsGPU to ease the transition of host-only kernels to NGP kernels. The *GPU classes will be merged with the original classes ScratchViews and ElemDataRequests once the transition of the core Kernel classes are complete.

---
+++ include/ElemDataRequestsGPU.h
@@ -0,0 +1,189 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+
+#ifndef ElemDataRequestsGPU_h
+#define ElemDataRequestsGPU_h
+
+#include <KokkosInterface.h>
+#include <Kokkos_Core.hpp>
+#include <ElemDataRequests.h>
+#include <FieldTypeDef.h>
+#include <stk_ngp/Ngp.hpp>
+
+namespace sierra{
+namespace nalu{
+
+struct FieldInfoNGP {
+  FieldInfoNGP(const stk::mesh::FieldBase* fld, unsigned scalars)
+  : field(fld->get_mesh(), *fld), scalarsDim1(scalars), scalarsDim2(0)
+  {}  
+  FieldInfoNGP(const stk::mesh::FieldBase* fld, unsigned tensorDim1, unsigned tensorDim2)
+  : field(fld->get_mesh(), *fld), scalarsDim1(tensorDim1), scalarsDim2(tensorDim2)
+  {}  
+  KOKKOS_FUNCTION
+  FieldInfoNGP(const FieldInfoNGP& rhs)
+  : field(rhs.field), scalarsDim1(rhs.scalarsDim1), scalarsDim2(rhs.scalarsDim2)
+  {}
+  KOKKOS_FUNCTION
+  FieldInfoNGP()
+  : field(), scalarsDim1(0), scalarsDim2(0)
+  {}
+
+  NGPDoubleFieldType field;
+  unsigned scalarsDim1;
+  unsigned scalarsDim2;
+};
+
+class ElemDataRequestsGPU
+{
+public:
+  typedef Kokkos::View<COORDS_TYPES*, Kokkos::LayoutRight, MemSpace> CoordsTypesView;
+  typedef Kokkos::View<ELEM_DATA_NEEDED*, Kokkos::LayoutRight, MemSpace> DataEnumView;
+  typedef Kokkos::View<NGPDoubleFieldType*, Kokkos::LayoutRight, MemSpace> FieldView;
+  typedef Kokkos::View<FieldInfoNGP*, Kokkos::LayoutRight, MemSpace> FieldInfoView;
+
+  ElemDataRequestsGPU(const ElemDataRequests& dataReq, unsigned totalFields)
+    : dataEnums(),
+      hostDataEnums(),
+      coordsFields_(),
+      hostCoordsFields_(),
+      coordsFieldsTypes_(),
+      hostCoordsFieldsTypes_(),
+      totalNumFields(totalFields),
+      fields(),
+      hostFields(),
+      meFC_(dataReq.get_cvfem_face_me()),
+      meSCS_(dataReq.get_cvfem_surface_me()),
+      meSCV_(dataReq.get_cvfem_volume_me()),
+      meFEM_(dataReq.get_fem_volume_me())
+  {
+    fill_host_data_enums(dataReq, CURRENT_COORDINATES);
+    fill_host_data_enums(dataReq, MODEL_COORDINATES);
+
+    fill_host_fields(dataReq);
+    fill_host_coords_fields(dataReq);
+
+    copy_to_device();
+  }
+
+  ~ElemDataRequestsGPU() {}
+
+  void add_cvfem_face_me(MasterElement *meFC)
+  { meFC_ = meFC; }
+
+  void add_cvfem_volume_me(MasterElement *meSCV)
+  { meSCV_ = meSCV; }
+
+  void add_cvfem_surface_me(MasterElement *meSCS)
+  { meSCS_ = meSCS; }
+
+  void add_fem_volume_me(MasterElement *meFEM)
+  { meFEM_ = meFEM; }
+
+  KOKKOS_FUNCTION
+  const DataEnumView& get_data_enums(const COORDS_TYPES cType) const
+  { return dataEnums[cType]; }
+
+  KOKKOS_FUNCTION
+  const FieldView& get_coordinates_fields() const
+  { return coordsFields_; }
+
+  KOKKOS_FUNCTION
+  const CoordsTypesView& get_coordinates_types() const
+  { return coordsFieldsTypes_; }
+
+  KOKKOS_FUNCTION
+  const FieldInfoView& get_fields() const { return fields; }  
+
+  KOKKOS_FUNCTION
+  unsigned get_total_num_fields() const { return totalNumFields; }
+
+  KOKKOS_FUNCTION
+  MasterElement *get_cvfem_face_me() const {return meFC_;}
+  KOKKOS_FUNCTION
+  MasterElement *get_cvfem_volume_me() const {return meSCV_;}
+  KOKKOS_FUNCTION
+  MasterElement *get_cvfem_surface_me() const {return meSCS_;}
+  KOKKOS_FUNCTION
+  MasterElement *get_fem_volume_me() const {return meFEM_;}
+
+private:
+  void copy_to_device()
+  {
+    if (hostDataEnums[CURRENT_COORDINATES].size() > 0) {
+      Kokkos::deep_copy(dataEnums[CURRENT_COORDINATES], hostDataEnums[CURRENT_COORDINATES]);
+    }
+    if (hostDataEnums[MODEL_COORDINATES].size() > 0) {
+      Kokkos::deep_copy(dataEnums[MODEL_COORDINATES], hostDataEnums[MODEL_COORDINATES]);
+    }
+    Kokkos::deep_copy(coordsFields_, hostCoordsFields_);
+    Kokkos::deep_copy(coordsFieldsTypes_, hostCoordsFieldsTypes_);
+    Kokkos::deep_copy(fields, hostFields);
+  }
+
+  void fill_host_data_enums(const ElemDataRequests& dataReq, COORDS_TYPES ctype)
+  {
+    if (dataReq.get_data_enums(ctype).size() > 0) {
+      dataEnums[ctype] = DataEnumView("DataEnumsCurrentCoords", dataReq.get_data_enums(ctype).size());
+      hostDataEnums[ctype] = Kokkos::create_mirror_view(dataEnums[ctype]);
+      unsigned i=0;
+      for(ELEM_DATA_NEEDED d : dataReq.get_data_enums(ctype)) {
+        hostDataEnums[ctype](i++) = d;
+      }
+    }
+  }
+
+  void fill_host_fields(const ElemDataRequests& dataReq)
+  {
+    fields = FieldInfoView("Fields", dataReq.get_fields().size());
+    hostFields = Kokkos::create_mirror_view(fields);
+    unsigned i = 0;
+    for(const FieldInfo& finfo : dataReq.get_fields()) {
+      hostFields(i++) = FieldInfoNGP(finfo.field, finfo.scalarsDim1, finfo.scalarsDim2);
+    }
+  }
+ 
+  void fill_host_coords_fields(const ElemDataRequests& dataReq)
+  {
+    coordsFields_ = FieldView("CoordsFields", dataReq.get_coordinates_map().size());
+    coordsFieldsTypes_ = CoordsTypesView("CoordsFieldsTypes", dataReq.get_coordinates_map().size());
+
+    hostCoordsFields_ = Kokkos::create_mirror_view(coordsFields_);
+    hostCoordsFieldsTypes_ = Kokkos::create_mirror_view(coordsFieldsTypes_);
+
+    unsigned i = 0;
+    for(auto iter : dataReq.get_coordinates_map()) {
+      hostCoordsFields_(i) = NGPDoubleFieldType(iter.second->get_mesh(), *iter.second);
+      hostCoordsFieldsTypes_(i) = iter.first;
+      ++i;
+    }
+  }
+
+  DataEnumView dataEnums[MAX_COORDS_TYPES];
+  DataEnumView::HostMirror hostDataEnums[MAX_COORDS_TYPES];
+
+  FieldView coordsFields_;
+  FieldView::HostMirror hostCoordsFields_;
+  CoordsTypesView coordsFieldsTypes_;
+  CoordsTypesView::HostMirror hostCoordsFieldsTypes_;
+
+  unsigned totalNumFields;
+  FieldInfoView fields;
+  FieldInfoView::HostMirror hostFields;
+
+  MasterElement *meFC_;
+  MasterElement *meSCS_;
+  MasterElement *meSCV_;
+  MasterElement *meFEM_;
+};
+
+} // namespace nalu
+} // namespace Sierra
+
+#endif
+
---
+++ include/MultiDimViews.h
@@ -0,0 +1,135 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#ifndef MultiDimViews_h
+#define MultiDimViews_h
+
+#include <KokkosInterface.h>
+
+namespace sierra {
+namespace nalu {
+
+struct NumNeededViews {
+  unsigned num1DViews;
+  unsigned num2DViews;
+  unsigned num3DViews;
+  unsigned num4DViews;
+};
+
+template<typename T, typename TEAMHANDLETYPE=DeviceTeamHandleType, typename SHMEM=DeviceShmem>
+class MultiDimViews {
+public:
+  using SharedMemView1D = SharedMemView<T*,SHMEM>;
+  using SharedMemView2D = SharedMemView<T**,SHMEM>;
+  using SharedMemView3D = SharedMemView<T***,SHMEM>;
+  using SharedMemView4D = SharedMemView<T****,SHMEM>;
+
+  KOKKOS_FUNCTION
+  MultiDimViews(const TEAMHANDLETYPE& team,
+                unsigned maxOrdinal,
+                const NumNeededViews& numNeededViews)
+  : ordinals(get_shmem_view_1D<int,TEAMHANDLETYPE,SHMEM>(team, maxOrdinal+1)),
+    views_1D(), views_1D_size(0),
+    views_2D(), views_2D_size(0),
+    views_3D(), views_3D_size(0),
+    views_4D(), views_4D_size(0)
+  {
+    for(unsigned i=0; i<numNeededViews.num1DViews; ++i) { new (&views_1D[i]) SharedMemView1D; }
+    for(unsigned i=0; i<numNeededViews.num2DViews; ++i) { new (&views_2D[i]) SharedMemView2D; }
+    for(unsigned i=0; i<numNeededViews.num3DViews; ++i) { new (&views_3D[i]) SharedMemView3D; }
+    for(unsigned i=0; i<numNeededViews.num4DViews; ++i) { new (&views_4D[i]) SharedMemView4D; }
+  }
+
+  KOKKOS_FUNCTION
+  static size_t bytes_needed(unsigned maxOrdinal, const NumNeededViews& numNeededViews)
+  {
+    return (maxOrdinal+1)*sizeof(unsigned)
+          + numNeededViews.num1DViews*sizeof(SharedMemView1D)
+          + numNeededViews.num2DViews*sizeof(SharedMemView2D)
+          + numNeededViews.num3DViews*sizeof(SharedMemView3D)
+          + numNeededViews.num4DViews*sizeof(SharedMemView4D)
+          + sizeof(SharedMemView<int*,SHMEM>)
+          + sizeof(SharedMemView<SharedMemView1D >)
+          + sizeof(SharedMemView<SharedMemView2D >)
+          + sizeof(SharedMemView<SharedMemView3D >)
+          + sizeof(SharedMemView<SharedMemView4D >);
+  }
+
+  KOKKOS_FUNCTION
+  SharedMemView1D& get_scratch_view_1D(unsigned ordinal)
+  {
+    SharedMemView1D& vref = views_1D[ordinals(ordinal)];
+    return vref;
+  }
+
+  KOKKOS_FUNCTION
+  SharedMemView2D& get_scratch_view_2D(unsigned ordinal)
+  {
+    return views_2D[ordinals(ordinal)];
+  }
+
+  KOKKOS_FUNCTION
+  SharedMemView3D& get_scratch_view_3D(unsigned ordinal)
+  {
+    return views_3D[ordinals(ordinal)];
+  }
+
+  KOKKOS_FUNCTION
+  SharedMemView4D& get_scratch_view_4D(unsigned ordinal)
+  {
+    return views_4D[ordinals(ordinal)];
+  }
+
+  KOKKOS_FUNCTION
+  void add_1D_view(unsigned ordinal, const SharedMemView1D& view)
+  {
+    views_1D[views_1D_size] = view;
+    ordinals(ordinal) = views_1D_size;
+    ++views_1D_size;
+  }
+
+  KOKKOS_FUNCTION
+  void add_2D_view(unsigned ordinal, const SharedMemView2D& view)
+  {
+    views_2D[views_2D_size] = view;
+    ordinals(ordinal) = views_2D_size;
+    ++views_2D_size;
+  }
+
+  KOKKOS_FUNCTION
+  void add_3D_view(unsigned ordinal, const SharedMemView3D& view)
+  {
+    views_3D[views_3D_size] = view;
+    ordinals(ordinal) = views_3D_size;
+    ++views_3D_size;
+  }
+
+  KOKKOS_FUNCTION
+  void add_4D_view(unsigned ordinal, const SharedMemView4D& view)
+  {
+    views_4D[views_4D_size] = view;
+    ordinals(ordinal) = views_4D_size;
+    ++views_4D_size;
+  }
+
+public:
+  SharedMemView<int*,SHMEM> ordinals;
+  SharedMemView1D views_1D[50];
+  unsigned views_1D_size;
+  SharedMemView2D views_2D[50];
+  unsigned views_2D_size;
+  SharedMemView3D views_3D[50];
+  unsigned views_3D_size;
+  SharedMemView4D views_4D[50];
+  unsigned views_4D_size;
+};
+
+}//namespace nalu
+}//namespace sierra
+
+#endif
+
