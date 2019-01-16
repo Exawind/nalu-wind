6c094ee39cad4161b0a69323ba8bddba4fc34473
Jon Rood <jon.rood@nrel.gov>
2019-01-16 16:27:30 -0700

    Updating instructions for building nalu-wind with spack.

--- docs/source/user/do-config-nalu-wind.sh
+++ docs/source/user/do-config-nalu-wind.sh
@@ -8,53 +8,29 @@
 CXX_COMPILER=mpicxx
 C_COMPILER=mpicc
 FORTRAN_COMPILER=mpifort
-FLAGS="-O2 -march=skylake -mtune=skylake"
+FLAGS="-O2 -march=native -mtune=native"
 OVERSUBSCRIBE_FLAGS="--use-hwthread-cpus --oversubscribe"
   
 set -e
 
-cmd() {
-  echo "+ $@"
-  eval "$@"
-}
-
-# Set up environment on Eagle
-cmd "module purge"
-cmd "module unuse ${MODULEPATH}"
-cmd "module use /nopt/nrel/ecom/hpacf/compilers/modules"
-cmd "module use /nopt/nrel/ecom/hpacf/utilities/modules"
-cmd "module use /nopt/nrel/ecom/hpacf/software/modules/gcc-7.3.0"
-cmd "module load gcc/7.3.0"
-cmd "module load python"
-cmd "module load git"
-cmd "module load binutils"
-cmd "module load openmpi"
-cmd "module load netlib-lapack"
-cmd "module load openfast"
-cmd "module load hypre"
-cmd "module load tioga"
-cmd "module load yaml-cpp"
-cmd "module load cmake"
-cmd "module load trilinos"
-cmd "module load fftw"
-cmd "module list"
+TRILINOS_ROOT_DIR=${NALU_ROOT_DIR}/install/trilinos
+YAML_CPP_ROOT_DIR=${NALU_ROOT_DIR}/install/yaml-cpp
 
 # Clean before cmake configure
 set +e
-cmd "rm -rf CMakeFiles"
-cmd "rm -f CMakeCache.txt"
+rm -rf CMakeFiles
+rm -f CMakeCache.txt
 set -e
-
-cmd "which cmake"
-cmd "which mpirun"
 
 # Extra TPLs that can be included in the cmake configure:
 #  -DENABLE_PARAVIEW_CATALYST:BOOL=ON \
 #  -DPARAVIEW_CATALYST_INSTALL_PATH:PATH=${CATALYST_IOSS_ADAPTER_ROOT_DIR} \
 #  -DENABLE_OPENFAST:BOOL=ON \
 #  -DOpenFAST_DIR:PATH=${OPENFAST_ROOT_DIR} \
-#  -DENABLE_FFTW:BOOL=ON \
-#  -DFFTW_DIR:PATH=${FFTW_ROOT_DIR} \
+#  -DENABLE_HYPRE:BOOL=ON \
+#  -DHYPRE_DIR:PATH=${HYPRE_ROOT_DIR} \
+#  -DENABLE_TIOGA:BOOL=ON \
+#  -DTIOGA_DIR:PATH=${TIOGA_ROOT_DIR} \
 
 (set -x; cmake \
   -DCMAKE_CXX_COMPILER:STRING=${CXX_COMPILER} \
@@ -69,10 +45,6 @@ cmd "which mpirun"
   -DMPIEXEC_PREFLAGS:STRING="${OVERSUBSCRIBE_FLAGS}" \
   -DTrilinos_DIR:PATH=${TRILINOS_ROOT_DIR} \
   -DYAML_DIR:PATH=${YAML_CPP_ROOT_DIR} \
-  -DENABLE_HYPRE:BOOL=ON \
-  -DHYPRE_DIR:PATH=${HYPRE_ROOT_DIR} \
-  -DENABLE_TIOGA:BOOL=ON \
-  -DTIOGA_DIR:PATH=${TIOGA_ROOT_DIR} \
   -DCMAKE_BUILD_TYPE:STRING=RELEASE \
   -DENABLE_DOCUMENTATION:BOOL=OFF \
   -DENABLE_TESTS:BOOL=ON \
