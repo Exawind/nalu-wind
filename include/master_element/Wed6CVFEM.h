426fb0d1d3f4566c4fb4ce307c745963bafc9160
James Overfelt <jroverf@sandia.gov>
2019-03-04 15:09:59 -0700

    Convert Wed6CVFEM to new format.

--- include/master_element/Wed6CVFEM.h
+++ include/master_element/Wed6CVFEM.h
@@ -69,6 +69,33 @@ public:
     const int &npts,
     const double *par_coord,
     double* shape_fcn);
+private:
+  const int nDim_ = AlgTraits::nDim_;
+  const int nodesPerElement_ = AlgTraits::nodesPerElement_; 
+  const int numIntPoints_ = AlgTraits::numScvIp_; 
+
+  // define ip node mappings
+  const int ipNodeMap_[6] = {0, 1, 2, 3, 4, 5};
+
+  // standard integration location
+  const double eleven18ths = 11.0/18.0;
+  const double seven36ths = 7.0/36.0;
+  const double intgLoc_[18] = {
+   seven36ths,   seven36ths,   -0.5, // vol 0
+   eleven18ths,  seven36ths,   -0.5, // vol 1
+   seven36ths,   eleven18ths,  -0.5, // vol 2
+   seven36ths,   seven36ths,   0.5,  // vol 3
+   eleven18ths,  seven36ths,   0.5,  // vol 4
+   seven36ths,   eleven18ths,  0.5}; // vol 5
+
+  // shifted
+  const double intgLocShift_[18] = {
+   0.0,   0.0,  -1.0, // vol 0
+   1.0,   0.0,  -1.0, // vol 1
+   0.0,   1.0,  -1.0, // vol 2
+   0.0,   0.0,   1.0, // vol 3
+   1.0,   0.0,  1.0,  // vol 4
+   0.0,   1.0,  1.0}; // vol 5
 };
 
 // Wedge 6 subcontrol surface
@@ -232,6 +259,10 @@ public:
 
   const int* side_node_ordinals(int sideOrdinal) final;
 private:
+  const int nDim_ = AlgTraits::nDim_;
+  const int nodesPerElement_ = AlgTraits::nodesPerElement_; 
+  const int numIntPoints_ = AlgTraits::numScsIp_; 
+
   const int sideNodeOrdinals_[18] = {
       0, 1, 4, 3, // ordinal 0
       1, 2, 5, 4, // ordinal 1
@@ -239,6 +270,106 @@ private:
       0, 2, 1,    // ordinal 3
       3, 4, 5     // ordinal 4
   };
+
+  // define L/R mappings
+  const int lrscv_[18] = {
+   0,  1, 
+   1,  2, 
+   0,  2, 
+   3,  4, 
+   4,  5, 
+   3,  5, 
+   0,  3, 
+   1,  4, 
+   2,  5};
+
+  // elem-edge mapping from ip
+  const int scsIpEdgeOrd_[AlgTraits::numScsIp_] = {
+   0,  1,  2, 
+   3,  4,  5, 
+   6,  7,  8};
+
+  // define opposing node
+  const int oppNode_[20] = {
+   2, 2, 5, 5,   // face 0: nodes 0,1,4,3
+   0, 0, 3, 3,   // face 1: nodes 1,2,5,4
+   1, 4, 4, 1,   // face 2: nodes 0,3,5,2
+   3, 5, 4, -1,  // face 3: nodes 0,2,1  
+   0, 1, 2, -1}; // face 4: nodes 3,4,5  
+
+  // define opposing face
+  const int oppFace_[20] = {
+       2,  1,  4,   5,    // face 0: nodes 0,1,4,3
+       0,  2,  5,   3,    // face 1: nodes 1,2,5,4
+       0,  3,  4,   1,    // face 2: nodes 0,3,5,2
+       6,  8,  7,  -1,   // face 3: nodes 0,2,1  
+       6,  7,  8,  -1};  // face 4: nodes 3,4,5  
+
+  // standard integration location
+  const double oneSixth = 1.0/6.0;
+  const double five12th = 5.0/12.0;
+  const double eleven18th = 11.0/18.0;
+  const double seven36th = 7.0/36.0;
+
+  const double intgLoc_[27] = {
+    five12th,   oneSixth,  -0.50, // surf 1    1->2
+    five12th,   five12th,  -0.50, // surf 2    2->3
+    oneSixth,   five12th,  -0.50, // surf 3    1->3
+    five12th,   oneSixth,   0.50, // surf 4    4->5
+    five12th,   five12th,   0.50, // surf 5    5->6
+    oneSixth,   five12th,   0.50, // surf 6    4->6
+    seven36th,  seven36th,   0.00, // surf 7    1->4
+    eleven18th, seven36th,   0.00, // surf 8    2->5
+    seven36th,  eleven18th,  0.00}; // surf 9    3->6
+
+  // shifted
+  const double intgLocShift_[27] = {
+    0.50,   0.00,  -1.00, // surf 1    1->2
+    0.50,   0.50,  -1.00, // surf 2    2->3
+    0.00,   0.50,  -1.00, // surf 3    1->3
+    0.50,   0.00,   1.00, // surf 4    4->5
+    0.50,   0.50,   1.00, // surf 5    5->6
+    0.00,   0.50,   1.00, // surf 6    4->6
+    0.00,   0.00,   0.00, // surf 7    1->4
+    1.00,   0.00,   0.00, // surf 8    2->5
+    0.00,   1.00,   0.00};// surf 9    3->6
+
+  // exposed face
+  const double intgExpFace_[60] = {
+   0.25,        0.0,        -0.5,  // surf 0: nodes 0,1,4,3
+   0.75,        0.0,        -0.5,  // face 0, surf 1
+   0.75,        0.0,         0.5,  // face 0, surf 2
+   0.25,        0.0,         0.5,  // face 0, surf 3
+   0.75,        0.25,        -0.5, // surf 1: nodes 1,2,5,4
+   0.25,        0.75,        -0.5, // face 1, surf 1
+   0.25,        0.75,         0.5, // face 1, surf 2
+   0.75,        0.25,         0.5, // face 1, surf 3
+   0.0,         0.25,        -0.5, // surf 2: nodes 0,3,5,2
+   0.0,         0.25,         0.5, // face 2, surf 1 
+   0.0,         0.75,         0.5, // face 2, surf 2
+   0.0,         0.75,        -0.5, // face 2, surf 3
+   seven36th,   seven36th,   -1.0, // surf 3: nodes 0,2,1
+   seven36th,   eleven18th,  -1.0, // face 3, surf 1
+   eleven18th,  seven36th,   -1.0, // face 3, surf 2
+   0.0,         0.0,          0.0, // (blank)
+   seven36th,   seven36th,   1.0,  // surf 4: nodes 3,4,5
+   eleven18th,  seven36th,   1.0,  // face 4, surf 1
+   seven36th,   eleven18th,  1.0,  // face 4, surf 2
+   0.0,         0.0,         0.0}; // (blank)
+
+  // boundary integration point ip node mapping (ip on an ordinal to local node number)
+  const int ipNodeMap_[20]  = { // 4 ips (pick quad) * 5 faces
+   0,  1,  4,  3,  // face 0
+   1,  2,  5,  4,  // face 1
+   0,  3,   5,  2, // face 2
+    0,  2,  1,  0, // face 3 empty
+    3,  4,  5,  0};// face 4 empty
+
+  // ordinal to vector offset map.  Really only convenient for the wedge.
+  const int sideOffset_[5] = {0, 4, 8, 12, 15};
+
+  double intgExpFaceShift_[54]; // no blanked entries
+
 };
 
 
