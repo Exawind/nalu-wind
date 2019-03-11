5eca0f3796b232729ba92215e664587952920df9
James Overfelt <jroverf@sandia.gov>
2019-03-11 13:33:04 -0600

    Split the 3D triangle into its own source file.

---
+++ include/master_element/Tri33DCVFEM.h
@@ -0,0 +1,90 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+
+#ifndef Tri33DCVFEM_h  
+#define Tri33DCVFEM_h  
+
+#include <master_element/MasterElement.h>
+
+#include <AlgTraits.h>
+
+// NGP-based includes
+#include "SimdInterface.h"
+#include "KokkosInterface.h"
+
+#include <array>
+#include <cstdlib>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+
+namespace sierra{
+namespace nalu{
+
+// 3D Tri 3
+class Tri3DSCS : public MasterElement
+{
+public:
+
+  Tri3DSCS();
+  virtual ~Tri3DSCS();
+
+  using AlgTraits = AlgTraitsTri3;
+  using MasterElement::determinant;
+  using MasterElement::shape_fcn;
+  using MasterElement::shifted_shape_fcn;
+
+  const int * ipNodeMap(int ordinal = 0);
+
+  void determinant(
+    const int nelem,
+    const double *coords,
+    double *areav,
+    double * error );
+
+  void shape_fcn(
+     double *shpfc);
+
+   void shifted_shape_fcn(
+     double *shpfc);
+
+   void tri_shape_fcn(
+     const int &npts,
+     const double *par_coord,
+     double* shape_fcn);
+
+   double isInElement(
+     const double *elemNodalCoord,
+     const double *pointCoord,
+     double *isoParCoord);
+
+  double parametric_distance(
+    const std::array<double,3> &x);
+
+  void interpolatePoint(
+    const int &nComp,
+    const double *isoParCoord,
+    const double *field,
+    double *result);
+
+  void general_shape_fcn(
+    const int numIp,
+    const double *isoParCoord,
+    double *shpfc);
+
+  void general_normal(
+    const double *isoParCoord,
+    const double *coords,
+    double *normal);
+};
+
+} // namespace nalu
+} // namespace Sierra
+
+#endif
