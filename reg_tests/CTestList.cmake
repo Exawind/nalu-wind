a52da0873e3fdab386b6fa97bc2d362a8888898c
Shreyas Ananthan <shreyas@umd.edu>
2019-03-15 07:58:35 -0600

    Add appropriate guards for oversetRotCylinder
    Only run the tests with TIOGA and Hypre both enabled.

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -155,9 +155,12 @@ if(ENABLE_OPENFAST)
    add_subdirectory(test_files/nrel5MWactuatorLine)
 endif(ENABLE_OPENFAST)
 if(ENABLE_TIOGA)
-  add_test_r(oversetRotCylinder 4)
   add_test_r(oversetSphereTIOGA 8)
 endif(ENABLE_TIOGA)
+
+if (ENABLE_TIOGA AND ENABLE_HYPRE)
+  add_test_r(oversetRotCylinder 4)
+endif()
 
 #=============================================================================
 # Convergence tests
