fd0a87f232959a7a0367fb133fbd4bedf1d0a53b
Shreyas Ananthan <shreyas@umd.edu>
2019-03-24 13:14:56 -0600

    ABL terrain treatment in WallDistanceEqSys (#158)
    * Don't impose Dirichlet BC for WallDistEqSys on ABL terrain boundaries
    
    * Add user option to force computation of WallDistance on initialization
    
    This option is useful when "restarting" from a mapped exodus restart
    database, e.g., a wind-farm mesh where the ABL precursor solution as
    mapped as a pre-processing step.

--- src/WallDistEquationSystem.C
+++ src/WallDistEquationSystem.C
@@ -86,6 +86,7 @@ WallDistEquationSystem::load(const YAML::Node& node)
   EquationSystem::load(node);
 
   get_if_present(node, "update_frequency", updateFreq_, updateFreq_);
+  get_if_present(node, "force_init_on_restart", forceInitOnRestart_, forceInitOnRestart_);
 }
 
 void
@@ -275,7 +276,7 @@ void
 WallDistEquationSystem::register_wall_bc(
   stk::mesh::Part* part,
   const stk::topology&,
-  const WallBoundaryConditionData&)
+  const WallBoundaryConditionData& wallBCData)
 {
   const AlgorithmType algType = WALL;
 
@@ -305,8 +306,14 @@ WallDistEquationSystem::register_wall_bc(
                              stk::topology::NODE_RANK);
   bcDataAlg_.push_back(auxAlg);
 
-  // Dirichlet BC
-  {
+  // For terrain BC, the wall distance calculations must not compute the
+  // distance normal to this wall, but must compute distance from the nearest
+  // turbine, so we will disable Dirichlet for the terrain walls.
+  WallUserData userData = wallBCData.userData_;
+  const bool ablWallFunctionActivated = userData.ablWallFunctionApproach_;
+
+  // Apply Dirichlet BC on non-ABL wall boundaries
+  if (!ablWallFunctionActivated) {
     auto it = solverAlgDriver_->solverDirichAlgMap_.find(algType);
     if (it == solverAlgDriver_->solverDirichAlgMap_.end()) {
       DirichletBC* theAlg
@@ -401,7 +408,12 @@ WallDistEquationSystem::initialize()
   // Reset init flag if this is a restarted simulation. The wall distance field
   // is available from the restart file, so we only want to recompute it at
   // user-specified frequency.
-  isInit_ = !realm_.restarted_simulation();
+  //
+  // The user option can override this and force a recompute. This option is
+  // useful when "restarting" from a mapped file, e.g., wind-farm mesh where the
+  // ABL precursor solution was mapped and is used to initialize the solution
+  // using restart section in the input file.
+  isInit_ = forceInitOnRestart_ || !realm_.restarted_simulation();
 }
 
 void
