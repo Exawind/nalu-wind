18551c9ac4bfea2a33457e08799a46962e9c693d
Shreyas Ananthan <shreyas@umd.edu>
2019-03-26 11:18:42 -0600

    Use global node IDs to minimize fringe/field mismatch (#156)
    Pass global Node IDs to TIOGA API for use in determining unique nodes
    across processor interface and minimize the fringe/field mismatches.

--- src/overset/TiogaBlock.C
+++ src/overset/TiogaBlock.C
@@ -485,6 +485,9 @@ void TiogaBlock::register_block(TIOGA::tioga& tg)
     num_cells_.data(),  // Number of cells for each topology
     tioga_conn_,        // Element node connectivity information
     elemid_map_.data()  // Global ID for the element array
+#ifdef TIOGA_HAS_NODEGID
+    ,nodeid_map_.data() // Global ID for the node array
+#endif
   );
   // Indicate that we want element IBLANK information returned
   tg.set_cell_iblank(meshtag_, iblank_cell_.data());
