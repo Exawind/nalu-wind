5618efafc926d3cd214914fa42ba3ded30562b58
Shreyas Ananthan <shreyas@umd.edu>
2019-04-02 15:45:07 -0600

    MasterElement creation/access on GPUs via Kernels
    Add unit-test to demonstrate creation of MasterElement on device within
    Kernel constructor and calling virtual methods on the MasterElement
    instance from within Kernel::execute method.

--- unit_tests/ngp_kernels/UTNgpKernelUtils.h
+++ unit_tests/ngp_kernels/UTNgpKernelUtils.h
@@ -37,10 +37,9 @@ public:
     velocity_    = sierra::nalu::get_field_ordinal(meta, "velocity");
     pressure_    = sierra::nalu::get_field_ordinal(meta, "pressure");
 
-    auto* meSCS =
-      sierra::nalu::MasterElementRepo::get_surface_master_element(stk::topology::HEX_8);
+    meSCS_ = sierra::nalu::MasterElementRepo::get_surface_master_element<AlgTraits>();
 
-    dataReq.add_cvfem_surface_me(meSCS);
+    dataReq.add_cvfem_surface_me(meSCS_);
 
     dataReq.add_coordinates_field(coordinates_, AlgTraits::nDim_,
                                   sierra::nalu::CURRENT_COORDINATES);
@@ -64,6 +63,8 @@ private:
   unsigned coordinates_ {stk::mesh::InvalidOrdinal};
   unsigned velocity_    {stk::mesh::InvalidOrdinal};
   unsigned pressure_    {stk::mesh::InvalidOrdinal};
+
+  sierra::nalu::MasterElement* meSCS_;
 };
 
 template<typename AlgTraits>
@@ -86,10 +87,15 @@ TestContinuityKernel<AlgTraits>::execute(
   sierra::nalu::SharedMemView<DoubleType*, ShmemType>& rhs,
   sierra::nalu::ScratchViews<double, TeamType, ShmemType>& scratchViews)
 {
+  // Get the integration point to node mapping
+  const int* ipNodeMap = meSCS_->ipNodeMap(3);
+
   auto& v_velocity = scratchViews.get_scratch_view_2D(velocity_);
   auto& v_pressure = scratchViews.get_scratch_view_1D(pressure_);
 
   rhs(0) = v_velocity(0, 0) + v_pressure(0);
+
+  printf("ipNodeMap[2] = %d; expected = 7\n", ipNodeMap[2]);
 }
 
 } // unit_test_ngp_kernels
