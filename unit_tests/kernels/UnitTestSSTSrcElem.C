99003b767a0d1717a6d6107fb8953057aef2ab7e
Jon Rood <jon.rood@nrel.gov>
2019-04-30 14:04:06 -0600

    Add NGP string to converted test.

--- unit_tests/kernels/UnitTestSSTSrcElem.C
+++ unit_tests/kernels/UnitTestSSTSrcElem.C
@@ -224,8 +224,7 @@ TEST_F(SSTKernelHex8Mesh, NGP_turb_kinetic_energy_sst_src_elem)
 #endif
 }
 
-
-TEST_F(SSTKernelHex8Mesh, turb_kinetic_energy_sst_des_src_elem)
+TEST_F(SSTKernelHex8Mesh, NGP_turb_kinetic_energy_sst_des_src_elem)
 {
 
   if (stk::parallel_machine_size(MPI_COMM_WORLD) > 1)
@@ -269,7 +268,6 @@ TEST_F(SSTKernelHex8Mesh, turb_kinetic_energy_sst_des_src_elem)
 #endif
 
 }
-
 
 TEST_F(SSTKernelHex8Mesh, NGP_specific_dissipation_rate_sst_src_elem)
 {
