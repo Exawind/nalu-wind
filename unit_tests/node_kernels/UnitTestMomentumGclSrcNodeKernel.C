5664c766fa317f7f7217bb9bcc98f35e22660f99
Jon Rood <jon.rood@nrel.gov>
2019-07-08 12:01:23 -0600

    Changing momentum gcl node kernel test name and adding gamma values and dt values to unit test.

--- unit_tests/node_kernels/UnitTestMomentumGclSrcNodeKernel.C
+++ unit_tests/node_kernels/UnitTestMomentumGclSrcNodeKernel.C
@@ -14,18 +14,28 @@
 
 #include <vector>
 
-TEST_F(MomentumNodeHex8Mesh, NGP_momentum_gcl_src)
+TEST_F(MomentumNodeHex8Mesh, NGP_momentum_gcl_node)
 {
   // Only execute for 1 processor runs
   if (bulk_.parallel_size() > 1) return;
 
   fill_mesh_and_init_fields();
 
+  sierra::nalu::TimeIntegrator timeIntegrator;
+  timeIntegrator.timeStepN_ = 0.1;
+  timeIntegrator.timeStepNm1_ = 0.1;
+  timeIntegrator.gamma1_ = 1.0;
+  timeIntegrator.gamma2_ = 0.0;
+  timeIntegrator.gamma3_ = 0.0;
+
   unit_test_utils::NodeHelperObjects helperObjs(
     bulk_, stk::topology::HEX_8, 3, partVec_[0]);
 
+  helperObjs.realm.timeIntegrator_ = &timeIntegrator;
+
   helperObjs.nodeAlg->add_kernel<sierra::nalu::MomentumGclSrcNodeKernel>(bulk_);
 
+  helperObjs.print_lhs_and_rhs();
   helperObjs.execute();
 
 #ifndef KOKKOS_ENABLE_CUDA
@@ -34,7 +44,7 @@ TEST_F(MomentumNodeHex8Mesh, NGP_momentum_gcl_src)
   EXPECT_EQ(helperObjs.linsys->rhs_.extent(0), 24u);
   EXPECT_EQ(helperObjs.linsys->numSumIntoCalls_, 8u);
 
-  unit_test_kernel_utils::expect_all_near(helperObjs.linsys->rhs_, 1.25, 1.0e-12);
-  unit_test_kernel_utils::expect_all_near<24>(helperObjs.linsys->lhs_, 0.0, 1.0e-12);
+  unit_test_kernel_utils::expect_all_near(helperObjs.linsys->rhs_, 0.0);
+  unit_test_kernel_utils::expect_all_near<24>(helperObjs.linsys->lhs_, 0.0);
 #endif
 }
