d81887e0c9027d9657dc4bd17d9a8ec4a14b6560
Jon Rood <jon.rood@nrel.gov>
2019-08-09 10:29:38 -0600

    Adding readthedocs.yml configuration file.

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Nalu-Wind'
-copyright = u'2018, Nalu-Wind Development Team'
+copyright = u'2019, Nalu-Wind Development Team'
 author = u'Nalu-Wind Team'
 
 # The version info for the project you're documenting, acts as replacement for
