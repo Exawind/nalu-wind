f30e555e4d27a61b4be0c82f8cadf5de15b588cf
James Overfelt <jroverf@sandia.gov>
2019-08-13 12:36:13 -0600

    More conservative formulation, from a C++ language standard point of view.

--- include/KokkosInterface.h
+++ include/KokkosInterface.h
@@ -15,7 +15,7 @@
 #include <Kokkos_Core.hpp>
 
 #ifdef KOKKOS_ENABLE_CUDA
-#define NALU_ALIGNED alignof(std::max_align_t)
+#define NALU_ALIGNED alignas(sizeof(long double))
 #else
 #define NALU_ALIGNED alignas(KOKKOS_MEMORY_ALIGNMENT)
 #endif
