44163f139519de3cd93b0811bdf68e02a24e9a45
Jon Rood <jon.rood@nrel.gov>
2019-09-06 17:27:16 -0600

    Conversion of EnthalpyEffectiveDiffFluxCoeffAlgorithm for NGP (#340)
    * Adding rough draft of EnthalpyEffectiveDiffFluxCoeffAlgorithm for NGP.
    
    * Filling in EnthalpyABLKernelHex8Mesh for EnthalpyEffDiffFluxCoeffAlg unit test.
    
    * Removing unnecessary namespace.
    
    * Fixing some formatting.

---
+++ src/ngp_algorithms/EnthalpyEffDiffFluxCoeffAlg.C
@@ -0,0 +1,79 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2019 National Renewable Energy Laboratory.                  */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#include <ngp_algorithms/EnthalpyEffDiffFluxCoeffAlg.h>
+#include "ngp_utils/NgpLoopUtils.h"
+#include "ngp_utils/NgpTypes.h"
+#include "Realm.h"
+#include "utils/StkHelpers.h"
+
+#include <stk_mesh/base/MetaData.hpp>
+
+namespace sierra{
+namespace nalu{
+
+EnthalpyEffDiffFluxCoeffAlg::EnthalpyEffDiffFluxCoeffAlg(
+  Realm &realm,
+  stk::mesh::Part* part,
+  ScalarFieldType* thermalCond,
+  ScalarFieldType* specHeat,
+  ScalarFieldType* tvisc,
+  ScalarFieldType* evisc,
+  const double sigmaTurb,
+  const bool isTurbulent
+) : Algorithm(realm, part),
+    specHeatField_(specHeat),
+    thermalCond_(thermalCond->mesh_meta_data_ordinal()),
+    specHeat_(specHeat->mesh_meta_data_ordinal()),
+    tvisc_(tvisc->mesh_meta_data_ordinal()),
+    evisc_(evisc->mesh_meta_data_ordinal()),
+    invSigmaTurb_(1.0 / sigmaTurb),
+    isTurbulent_(isTurbulent)
+{}
+
+void
+EnthalpyEffDiffFluxCoeffAlg::execute()
+{
+  using Traits = nalu_ngp::NGPMeshTraits<ngp::Mesh>;
+
+  const auto& meta = realm_.meta_data();
+
+  stk::mesh::Selector sel = (
+    meta.locally_owned_part() | meta.globally_shared_part())
+    & stk::mesh::selectField(*specHeatField_);
+
+  const auto& meshInfo = realm_.mesh_info();
+  const auto ngpMesh = meshInfo.ngp_mesh();
+  const auto& fieldMgr = meshInfo.ngp_field_manager();
+  const auto thermalCond = fieldMgr.get_field<double>(thermalCond_);
+  const auto specHeat = fieldMgr.get_field<double>(specHeat_);
+  const auto tvisc = fieldMgr.get_field<double>(tvisc_);
+  auto evisc = fieldMgr.get_field<double>(evisc_);
+  const DblType invSigmaTurb = invSigmaTurb_;
+
+  if (isTurbulent_) {
+    nalu_ngp::run_entity_algorithm(
+      ngpMesh, stk::topology::NODE_RANK, sel,
+      KOKKOS_LAMBDA(const Traits::MeshIndex& meshIdx) {
+        evisc.get(meshIdx, 0) = (
+          thermalCond.get(meshIdx, 0) / specHeat.get(meshIdx, 0) +
+          tvisc.get(meshIdx, 0) * invSigmaTurb);
+      });
+  } else {
+    nalu_ngp::run_entity_algorithm(
+      ngpMesh, stk::topology::NODE_RANK, sel,
+      KOKKOS_LAMBDA(const Traits::MeshIndex& meshIdx) {
+        evisc.get(meshIdx, 0) = (
+          thermalCond.get(meshIdx, 0) / specHeat.get(meshIdx, 0));
+      });
+  }
+
+  evisc.modify_on_device();
+}
+
+} // namespace nalu
+} // namespace Sierra
