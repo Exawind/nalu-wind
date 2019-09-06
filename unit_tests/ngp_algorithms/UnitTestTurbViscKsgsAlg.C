38c5e1856936ca586189c1f86b8f75e0e6b0348a
Jon Rood <jon.rood@nrel.gov>
2019-09-06 16:08:34 -0600

    Convert TurbViscKsgsAlg to NGP.

---
+++ unit_tests/ngp_algorithms/UnitTestTurbViscKsgsAlg.C
@@ -0,0 +1,63 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2019 National Renewable Energy Laboratory.                  */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#include "kernels/UnitTestKernelUtils.h"
+#include "UnitTestHelperObjects.h"
+
+#include "ngp_algorithms/TurbViscKsgsAlg.h"
+
+TEST_F(KsgsKernelHex8Mesh, NGP_turb_visc_ksgs_alg)
+{
+  // Only execute for 1 processor runs
+  if (bulk_.parallel_size() > 1) return;
+
+  fill_mesh_and_init_fields();
+
+/*
+  stk::mesh::field_fill(0.0, *tke_);
+  stk::mesh::field_fill(1.0, *density_);
+  stk::mesh::field_fill(0.0, *tvisc_);
+  stk::mesh::field_fill(0.2, *dualNodalVolume_);
+*/
+  unit_test_utils::HelperObjects helperObjs(
+    bulk_, stk::topology::HEX_8, 1, partVec_[0]);
+
+  sierra::nalu::TurbViscKsgsAlg TurbViscKsgsAlg(
+    helperObjs.realm, partVec_[0], tke_, density_, tvisc_,
+    dualNodalVolume_);
+
+  TurbViscKsgsAlg.execute();
+
+  const auto& fieldMgr = helperObjs.realm.mesh_info().ngp_field_manager();
+  auto ngpTvisc = fieldMgr.get_field<double>(tvisc_->mesh_meta_data_ordinal());
+  ngpTvisc.modify_on_device();
+  ngpTvisc.sync_to_host();
+
+  {
+    std::vector<double> expectedValues = {
+      0.060528340469568467,
+      0.035577665873750018,
+      0.042163789612795696,
+      0.023265644281201679,
+      0.035577665873750018,
+      0.020912027311579463,
+      0.023265644281201679,
+      0.013122616366371343
+    };
+
+    const double tol = 1.0e-16;
+    stk::mesh::Selector sel = meta_.universal_part();
+    const auto& bkts = bulk_.get_buckets(stk::topology::NODE_RANK, sel);
+
+    int ii = 0;
+    for (const auto* b: bkts)
+      for (const auto node: *b) {
+        const double* tvisc = stk::mesh::field_data(*tvisc_, node);
+        EXPECT_NEAR(tvisc[0], expectedValues[ii++], tol);
+      }
+  }
+}
