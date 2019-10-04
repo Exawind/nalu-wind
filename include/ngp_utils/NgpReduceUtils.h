464b441b64cd2426c2596b80c9a807344cd0eb0b
Shreyas Ananthan <shreyas@umd.edu>
2019-10-04 09:12:41 -0600

    Add reducer types for stk::simd::Double

--- include/ngp_utils/NgpReduceUtils.h
+++ include/ngp_utils/NgpReduceUtils.h
@@ -8,7 +8,10 @@
 #ifndef NGPREDUCEUTILS_H
 #define NGPREDUCEUTILS_H
 
+#include <cfloat>
+
 #include "KokkosInterface.h"
+#include "SimdInterface.h"
 
 namespace sierra {
 namespace nalu {
@@ -61,11 +64,34 @@ using ArrayDbl2 = NgpReduceArray<double, 2>;
 using ArrayDbl3 = NgpReduceArray<double, 3>;
 using ArrayInt2 = NgpReduceArray<int, 2>;
 
+using ArraySimdDouble2 = NgpReduceArray<DoubleType, 2>;
+using ArraySimdDouble3 = NgpReduceArray<DoubleType, 3>;
+
 }  // nalu_ngp
 }  // nalu
 }  // sierra
 
 namespace Kokkos {
+
+template<>
+struct reduction_identity<sierra::nalu::DoubleType>
+{
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::DoubleType sum()
+  { return DoubleType(0.0); }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::DoubleType prod()
+  { return DoubleType(1.0); }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::DoubleType max()
+  { return DoubleType(-DBL_MAX); }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::DoubleType min()
+  { return DoubleType(DBL_MAX); }
+};
 
 template<>
 struct reduction_identity<sierra::nalu::nalu_ngp::ArrayDbl2>
@@ -89,6 +115,30 @@ struct reduction_identity<sierra::nalu::nalu_ngp::ArrayDbl3>
   KOKKOS_FORCEINLINE_FUNCTION
   static sierra::nalu::nalu_ngp::ArrayDbl3 prod()
   { return sierra::nalu::nalu_ngp::ArrayDbl3(1.0); }
+};
+
+template<>
+struct reduction_identity<sierra::nalu::nalu_ngp::ArraySimdDouble2>
+{
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::nalu_ngp::ArraySimdDouble2 sum()
+  { return sierra::nalu::nalu_ngp::ArraySimdDouble2(DoubleType(0.0)); }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::nalu_ngp::ArraySimdDouble2 prod()
+  { return sierra::nalu::nalu_ngp::ArraySimdDouble2(DoubleType(1.0)); }
+};
+
+template<>
+struct reduction_identity<sierra::nalu::nalu_ngp::ArraySimdDouble3>
+{
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::nalu_ngp::ArraySimdDouble3 sum()
+  { return sierra::nalu::nalu_ngp::ArraySimdDouble3(DoubleType(0.0)); }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  static sierra::nalu::nalu_ngp::ArraySimdDouble3 prod()
+  { return sierra::nalu::nalu_ngp::ArraySimdDouble3(DoubleType(1.0)); }
 };
 
 template<>
