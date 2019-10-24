e62d404803bc02119a07eb2af09271ae6518f279
ashesh2512 <36968394+ashesh2512@users.noreply.github.com>
2019-10-24 08:34:35 -0600

    Create NGP version of momentum outflow BC  (#421)
    * adding kernel for open bc for momentum
    
    * removal of setup function because it is not required for this kernel
    
    * adding unit test for MomentumOpenEdgeKernel
    
    * fix comparison of gold values in the unit test
    
    * remove solution options as member variable of class
    
    * some modifications to clean up the code

---
+++ include/edge_kernels/MomentumOpenEdgeKernel.h
@@ -0,0 +1,72 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2019 National Renewable Energy Laboratory.                  */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#ifndef MOMENTUMOPENEDGEKERNEL_h
+#define MOMENTUMOPENEDGEKERNEL_h
+
+#include "kernel/Kernel.h"
+#include "KokkosInterface.h"
+#include "FieldTypeDef.h"
+
+#include "stk_mesh/base/MetaData.hpp"
+#include "stk_mesh/base/Entity.hpp"
+
+namespace sierra{
+namespace nalu{
+
+class SolutionOptions;
+class TimeIntegrator;
+
+template<typename BcAlgTraits>
+class MomentumOpenEdgeKernel : public NGPKernel<MomentumOpenEdgeKernel<BcAlgTraits>>
+{
+public:
+
+  MomentumOpenEdgeKernel(
+    const stk::mesh::MetaData&,
+    SolutionOptions*,
+    ScalarFieldType*,
+    ElemDataRequests&,
+    ElemDataRequests&);
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  MomentumOpenEdgeKernel() = default;
+
+  KOKKOS_INLINE_FUNCTION
+  virtual ~MomentumOpenEdgeKernel() = default;
+
+  using Kernel::execute;
+
+  KOKKOS_FUNCTION
+  virtual void execute(
+    SharedMemView<DoubleType**, DeviceShmem>&,
+    SharedMemView<DoubleType*, DeviceShmem>&,
+    ScratchViews<DoubleType, DeviceTeamHandleType, DeviceShmem>&,
+    ScratchViews<DoubleType, DeviceTeamHandleType, DeviceShmem>&,
+    int);
+
+private:
+
+  const unsigned coordinates_      {stk::mesh::InvalidOrdinal};
+  const unsigned dudx_             {stk::mesh::InvalidOrdinal};
+  const unsigned exposedAreaVec_   {stk::mesh::InvalidOrdinal};
+  const unsigned openMassFlowRate_ {stk::mesh::InvalidOrdinal};
+  const unsigned velocityBc_       {stk::mesh::InvalidOrdinal};
+  const unsigned velocityNp1_      {stk::mesh::InvalidOrdinal};
+  const unsigned viscosity_        {stk::mesh::InvalidOrdinal};
+
+  const double includeDivU_;
+  const double nfEntrain_;
+
+  MasterElement* meFC_{nullptr};
+  MasterElement* meSCS_{nullptr};
+};
+
+} // namespace nalu
+} // namespace Sierra
+
+#endif /* MOMENTUMOPENEDGEKERNEL_h */
---
+++ unit_tests/edge_kernels/UnitTestMomentumOpenEdge.C
@@ -0,0 +1,83 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2019 National Renewable Energy Laboratory.                  */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#include "kernels/UnitTestKernelUtils.h"
+#include "UnitTestUtils.h"
+#include "UnitTestHelperObjects.h"
+
+#include "edge_kernels/MomentumOpenEdgeKernel.h"
+
+namespace {
+namespace hex8_golds  {
+static constexpr double rhs[24] = {
+  0, 0, 0, 0, 0.0063760611306953, 0, 0, 0,
+  0, -0.057266021854104, 0.0037477547003379, 0, 0, 0, 0, 0,
+  0.0063760611306953, 0, 0, 0, 0, -0.057266021854104, 0.0037477547003379, 0,
+};
+
+static constexpr double lhs[24][24] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {-0.014860258067112, 0, 0, -0.014860258067112, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, -0.044580774201335, 0, 0, -0.044580774201335, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -0.014860258067112, 0, 0, -0.014860258067112, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -0.044580774201335, 0, 0, -0.044580774201335, 0, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, 0, },
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.025, 0, 0, -0.025, },
+};
+}
+}
+
+TEST_F(MomentumKernelHex8Mesh, NGP_open_edge)
+{
+  if (bulk_.parallel_size() > 1) return;
+
+  const bool doPerturb = false;
+  const bool generateSidesets = true;
+  fill_mesh_and_init_fields(doPerturb, generateSidesets);
+
+  // Setup solution options for default advection kernel
+  solnOpts_.meshMotion_ = false;
+  solnOpts_.meshDeformation_ = false;
+  solnOpts_.externalMeshDeformation_ = false;
+
+  auto* part = meta_.get_part("surface_2");
+  bool isEdge = true;
+  unit_test_utils::FaceElemHelperObjects helperObjs(
+    bulk_, stk::topology::QUAD_4, stk::topology::HEX_8, 3, part, isEdge);
+
+  std::unique_ptr<sierra::nalu::Kernel> kernel(
+    new sierra::nalu::MomentumOpenEdgeKernel<
+      sierra::nalu::AlgTraitsQuad4Hex8>(
+      meta_, &solnOpts_, viscosity_,
+      helperObjs.assembleFaceElemSolverAlg->faceDataNeeded_,
+      helperObjs.assembleFaceElemSolverAlg->elemDataNeeded_));
+
+  helperObjs.assembleFaceElemSolverAlg->activeKernels_.push_back(kernel.get());
+
+  helperObjs.execute();
+
+  unit_test_kernel_utils::expect_all_near(
+    helperObjs.linsys->rhs_, hex8_golds::rhs, 1.0e-12);
+  unit_test_kernel_utils::expect_all_near<24>(
+    helperObjs.linsys->lhs_, hex8_golds::lhs, 1.0e-12);}
