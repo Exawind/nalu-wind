Exawind/nalu-wind

commit 63cb32e772ffc38d127abebeca7a97a6c7aa8188

Step 2 of coeff-applier cleanup. (#432)
* Step 2 of coeff-applier cleanup.

Make coeff-applier pointers be owned/managed by
LinearSystem. By default, get_coeff_applier returns a device pointer.
This also makes sense when running a non-cuda build, since the
host is the same as the device.
If we need a host pointer when running a cuda build, we can add a
get_host_coeff_applier.

Next step will be to try to move the extract-diagonal code into
something (a functor?) owned by SolverAlgorithm, and also put
the fix-overset code in that.

* Use std::unique_ptr for hostCoeffApplier.

include/AssembleEdgeSolverAlgorithm.h
@@ -64,8 +64,7 @@ public:
     const auto entityRank = entityRank_;
     const auto rhsSize = rhsSize_;
 
-    CoeffApplier* coeffApplier = eqSystem_->linsys_->get_coeff_applier();
-    CoeffApplier* deviceCoeffApplier = coeffApplier->device_pointer();
+    CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
 
     const auto nodesPerEntity = nodesPerEntity_;
 
@@ -97,9 +96,6 @@ public:
               smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
           });
       });
-
-      coeffApplier->free_device_pointer();
-      delete coeffApplier;
   }
 
 protected:

src/AssembleElemSolverAlgorithm.C
@@ -89,8 +89,7 @@ AssembleElemSolverAlgorithm::execute()
 
   auto ngpKernels = nalu_ngp::create_ngp_view<Kernel>(activeKernels_);
 
-  CoeffApplier* coeffApplier = eqSystem_->linsys_->get_coeff_applier();
-  CoeffApplier* deviceCoeffApplier = coeffApplier->device_pointer();
+  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
 
   double diagRelaxFactor = diagRelaxFactor_;
   int rhsSize = rhsSize_;
@@ -124,9 +123,6 @@ AssembleElemSolverAlgorithm::execute()
                   smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
 #endif
     });
-
-  coeffApplier->free_device_pointer();
-  delete coeffApplier;
 }
 
 } // namespace nalu

src/AssembleFaceElemSolverAlgorithm.C
@@ -92,8 +92,7 @@ AssembleFaceElemSolverAlgorithm::execute()
 
   const unsigned nodesPerEntity = nodesPerElem_;
   const unsigned numDof = numDof_;
-  CoeffApplier* coeffApplier = eqSystem_->linsys_->get_coeff_applier();
-  CoeffApplier* deviceCoeffApplier = coeffApplier->device_pointer();
+  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
   double diagRelaxFactor = diagRelaxFactor_;
 
   run_face_elem_algorithm(realm_.bulk_data(),
@@ -125,9 +124,6 @@ AssembleFaceElemSolverAlgorithm::execute()
         }
 #endif
     });
-
-    coeffApplier->free_device_pointer();
-    delete coeffApplier;
 }
 
 } // namespace nalu

src/AssembleNGPNodeSolverAlgorithm.C
@@ -91,8 +91,7 @@ AssembleNGPNodeSolverAlgorithm::execute()
   const stk::mesh::EntityRank entityRank = stk::topology::NODE_RANK;
   const int rhsSize = rhsSize_;
 
-  CoeffApplier* coeffApplier = eqSystem_->linsys_->get_coeff_applier();
-  CoeffApplier* deviceCoeffApplier = coeffApplier->device_pointer();
+  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
 
   const int nodesPerEntity = 1;
   const int bytes_per_team = 0;
@@ -134,9 +133,6 @@ AssembleNGPNodeSolverAlgorithm::execute()
             smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
         });
     });
-
-    coeffApplier->free_device_pointer();
-    delete coeffApplier;
 }
 
 }  // nalu

src/TpetraLinearSystem.C
@@ -1310,20 +1310,25 @@ void reset_rows(
 
 sierra::nalu::CoeffApplier* TpetraLinearSystem::get_coeff_applier()
 {
-  const bool extractDiagonal = equationSystem()->extractDiagonal_;
-  const unsigned diagFieldOrdinal = (extractDiagonal && equationSystem()->get_diagonal_field() !=nullptr) ?
-                    equationSystem()->get_diagonal_field()->mesh_meta_data_ordinal() : 0;
-  
-  NGPDoubleFieldType diagField;
-  if (extractDiagonal) {
-    diagField = realm_.ngp_field_manager().get_field<double>(diagFieldOrdinal);
+  if (hostCoeffApplier.get() == nullptr) {
+    const bool extractDiagonal = equationSystem()->extractDiagonal_;
+    const unsigned diagFieldOrdinal = (extractDiagonal && equationSystem()->get_diagonal_field() !=nullptr) ?
+                      equationSystem()->get_diagonal_field()->mesh_meta_data_ordinal() : 0;
+    
+    NGPDoubleFieldType diagField;
+    if (extractDiagonal) {
+      diagField = realm_.ngp_field_manager().get_field<double>(diagFieldOrdinal);
+    }
+   
+    hostCoeffApplier.reset(new TpetraLinSysCoeffApplier(ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
+                                        ownedLocalRhs_, sharedNotOwnedLocalRhs_,
+                                        entityToLID_, entityToColLID_,
+                                        maxOwnedRowId_, maxSharedNotOwnedRowId_, numDof_,
+                                        extractDiagonal, diagField, realm_.ngp_mesh()));
+    deviceCoeffApplier = hostCoeffApplier->device_pointer();
   }
 
-  return new TpetraLinSysCoeffApplier(ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
-                                      ownedLocalRhs_, sharedNotOwnedLocalRhs_,
-                                      entityToLID_, entityToColLID_,
-                                      maxOwnedRowId_, maxSharedNotOwnedRowId_, numDof_,
-                                      extractDiagonal, diagField, realm_.ngp_mesh());
+  return deviceCoeffApplier;
 }
 
 KOKKOS_FUNCTION

include/LinearSystem.h
@@ -79,7 +79,12 @@ public:
     EquationSystem *eqSys,
     LinearSolver *linearSolver);
 
-  virtual ~LinearSystem() {}
+  virtual ~LinearSystem() {
+    if (hostCoeffApplier.get() != nullptr) {
+      hostCoeffApplier->free_device_pointer();
+      deviceCoeffApplier = nullptr;
+    }
+  }
 
   static LinearSystem *create(Realm& realm, const unsigned numDof, EquationSystem *eqSys, LinearSolver *linearSolver);
 
@@ -154,7 +159,10 @@ public:
   virtual CoeffApplier* get_coeff_applier()
   {
 #ifndef KOKKOS_ENABLE_CUDA
-    return new DefaultHostOnlyCoeffApplier(*this);
+    if (hostCoeffApplier.get() == nullptr) {
+      hostCoeffApplier.reset(new DefaultHostOnlyCoeffApplier(*this));
+    }
+    return hostCoeffApplier.get();
 #else
     return nullptr;
 #endif
@@ -265,6 +273,9 @@ protected:
   double scaledNonLinearResidual_;
   bool recomputePreconditioner_;
   bool reusePreconditioner_;
+
+  std::unique_ptr<CoeffApplier> hostCoeffApplier;
+  CoeffApplier* deviceCoeffApplier = nullptr;
 
 public:
   bool provideOutput_;

src/FixPressureAtNodeAlgorithm.C
@@ -66,8 +66,7 @@ FixPressureAtNodeAlgorithm::execute()
   }
 
   // Reset LHS and RHS for this matrix
-  CoeffApplier* coeffApplier = eqSystem_->linsys_->get_coeff_applier();
-  CoeffApplier* deviceCoeffApplier = coeffApplier->device_pointer();
+  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
  
   ngp::Mesh ngpMesh = realm_.ngp_mesh();
   NGPDoubleFieldType ngpPressure = realm_.ngp_field_manager().get_field<double>(pressure_->mesh_meta_data_ordinal());
@@ -108,9 +107,6 @@ FixPressureAtNodeAlgorithm::execute()
       }
     });
   });
-
-  coeffApplier->free_device_pointer();
-  delete coeffApplier;
 }
 
 void

src/TpetraSegregatedLinearSystem.C
@@ -1179,20 +1179,25 @@ void reset_rows(
 
 sierra::nalu::CoeffApplier* TpetraSegregatedLinearSystem::get_coeff_applier()
 {
-  const bool extractDiagonal = equationSystem()->extractDiagonal_;
-  const unsigned diagFieldOrdinal = (extractDiagonal && equationSystem()->get_diagonal_field()!=nullptr) ?
-                    equationSystem()->get_diagonal_field()->mesh_meta_data_ordinal() : 0;
-
-  NGPDoubleFieldType diagField;
-  if (extractDiagonal) {
-    diagField = realm_.ngp_field_manager().get_field<double>(diagFieldOrdinal);
+  if (hostCoeffApplier.get() == nullptr) {
+    const bool extractDiagonal = equationSystem()->extractDiagonal_;
+    const unsigned diagFieldOrdinal = (extractDiagonal && equationSystem()->get_diagonal_field()!=nullptr) ?
+                      equationSystem()->get_diagonal_field()->mesh_meta_data_ordinal() : 0;
+  
+    NGPDoubleFieldType diagField;
+    if (extractDiagonal) {
+      diagField = realm_.ngp_field_manager().get_field<double>(diagFieldOrdinal);
+    }
+  
+    hostCoeffApplier.reset(new TpetraLinSysCoeffApplier(ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
+                                        ownedLocalRhs_, sharedNotOwnedLocalRhs_,
+                                        entityToLID_, entityToColLID_,
+                                        maxOwnedRowId_, maxSharedNotOwnedRowId_, numDof_,
+                                        extractDiagonal, diagField, realm_.ngp_mesh()));
+    deviceCoeffApplier = hostCoeffApplier->device_pointer();
   }
 
-  return new TpetraLinSysCoeffApplier(ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
-                                      ownedLocalRhs_, sharedNotOwnedLocalRhs_,
-                                      entityToLID_, entityToColLID_,
-                                      maxOwnedRowId_, maxSharedNotOwnedRowId_, numDof_,
-                                      extractDiagonal, diagField, realm_.ngp_mesh());
+  return deviceCoeffApplier;
 }
 
 KOKKOS_FUNCTION

commit d4dfd7de71b85486e22704e9e076588719926287

Fix segfaults on MacOS introduced by #432 and #434
Updates to Tpetra CoeffApplier code introduced segfaults on MacOS where the
ngp::Mesh instance stored within that object was deleted after
stk::mesh::BulkData was deleted. This commit simplies Tpetra CoeffApplier so
that it doesn't handle diagonal extraction anymore and just deals with sum_into
for the linear system.

This commit also restores the old behavior of SolverAlgorithm::apply_coeff for
all NGP assembly. For momentum solves, it will extract the diagonal term for use
with pressure Poisson solve and, for overset meshes, it restores the reset of
lhs/rhs coefficients for fringe and hole nodes.

include/AssembleEdgeSolverAlgorithm.h
@@ -64,7 +64,7 @@ public:
     const auto entityRank = entityRank_;
     const auto rhsSize = rhsSize_;
 
-    CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
+    auto coeffApplier = coeff_applier();
 
     const auto nodesPerEntity = nodesPerEntity_;
 
@@ -91,15 +91,7 @@ public:
 
             lambdaFunc(smdata, edgeIndex, nodeL, nodeR);
 
-#ifndef KOKKOS_ENABLE_CUDA
-            // TODO: Replace this with NGP version
-            if (realm_.hasOverset_)
-              reset_overset_rows(
-                realm_.meta_data(), eqSystem_->linsys_->numDof(), nodesPerEntity,
-                smdata.ngpElemNodes, smdata.rhs, smdata.lhs);
-#endif
-
-            (*deviceCoeffApplier)(
+            coeffApplier(
               nodesPerEntity, smdata.ngpElemNodes, smdata.scratchIds,
               smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
           });

include/SolverAlgorithm.h
@@ -19,8 +19,53 @@
 namespace sierra{
 namespace nalu{
 
+class CoeffApplier;
 class EquationSystem;
 class Realm;
+
+struct NGPApplyCoeff
+{
+  NGPApplyCoeff(EquationSystem*);
+
+  KOKKOS_INLINE_FUNCTION
+  NGPApplyCoeff() = default;
+
+  KOKKOS_INLINE_FUNCTION
+  ~NGPApplyCoeff() = default;
+
+  KOKKOS_FUNCTION
+  void operator()(
+    unsigned numMeshobjs,
+    const ngp::Mesh::ConnectedNodes& symMeshobjs,
+    const SharedMemView<int*,DeviceShmem> & scratchIds,
+    const SharedMemView<int*,DeviceShmem> & sortPermutation,
+    SharedMemView<double*,DeviceShmem> & rhs,
+    SharedMemView<double**,DeviceShmem> & lhs,
+    const char *trace_tag) const;
+
+  KOKKOS_FUNCTION
+  void extract_diagonal(
+    const unsigned nEntities,
+    const ngp::Mesh::ConnectedNodes& entities,
+    SharedMemView<double**, DeviceShmem>& lhs) const;
+
+  KOKKOS_FUNCTION
+  void reset_overset_rows(
+    const unsigned nEntities,
+    const ngp::Mesh::ConnectedNodes& entities,
+    SharedMemView<double*, DeviceShmem>&  rhs,
+    SharedMemView<double**, DeviceShmem>& lhs) const;
+
+  const ngp::Mesh ngpMesh_;
+  mutable ngp::Field<double> diagField_;
+  ngp::Field<int> iblankField_;
+
+  CoeffApplier* deviceSumInto_;
+
+  const unsigned nDim_{3};
+  const bool hasOverset_{false};
+  const bool extractDiagonal_{false};
+};
 
 class SolverAlgorithm : public Algorithm
 {
@@ -36,6 +81,9 @@ public:
   virtual void initialize_connectivity() = 0;
 
 protected:
+
+  NGPApplyCoeff coeff_applier()
+  { return NGPApplyCoeff(eqSystem_); }
 
   // Need to find out whether this ever gets called inside a modification cycle.
   void apply_coeff(

include/TpetraLinearSystem.h
@@ -173,9 +173,7 @@ public:
                              LinSys::LocalVector sharedNotOwnedLclRhs,
                              LinSys::EntityToLIDView entityLIDs,
                              LinSys::EntityToLIDView entityColLIDs,
-                             int maxOwnedRowId, int maxSharedNotOwnedRowId, unsigned numDof,
-                             bool extractDiagonal, NGPDoubleFieldType& diagField,
-                             const ngp::Mesh& ngpMesh)
+                             int maxOwnedRowId, int maxSharedNotOwnedRowId, unsigned numDof)
     : ownedLocalMatrix_(ownedLclMatrix),
       sharedNotOwnedLocalMatrix_(sharedNotOwnedLclMatrix),
       ownedLocalRhs_(ownedLclRhs),
@@ -183,8 +181,6 @@ public:
       entityToLID_(entityLIDs),
       entityToColLID_(entityColLIDs),
       maxOwnedRowId_(maxOwnedRowId), maxSharedNotOwnedRowId_(maxSharedNotOwnedRowId), numDof_(numDof),
-      extractDiagonal_(extractDiagonal), diagField_(diagField),
-      ngpMesh_(ngpMesh),
       devicePointer_(nullptr)
     {}
 
@@ -219,9 +215,6 @@ public:
     LinSys::EntityToLIDView entityToColLID_;
     int maxOwnedRowId_, maxSharedNotOwnedRowId_;
     unsigned numDof_;
-    bool extractDiagonal_;
-    NGPDoubleFieldType diagField_;
-    ngp::Mesh ngpMesh_;
     TpetraLinSysCoeffApplier* devicePointer_;
   };
 

src/AssembleElemSolverAlgorithm.C
@@ -88,8 +88,7 @@ AssembleElemSolverAlgorithm::execute()
     activeKernels_[i]->setup(*realm_.timeIntegrator_);
 
   auto ngpKernels = nalu_ngp::create_ngp_view<Kernel>(activeKernels_);
-
-  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
+  auto coeffApplier = coeff_applier();
 
   double diagRelaxFactor = diagRelaxFactor_;
   int rhsSize = rhsSize_;
@@ -105,23 +104,19 @@ AssembleElemSolverAlgorithm::execute()
         kernel->execute(smdata.simdlhs, smdata.simdrhs, smdata.simdPrereqData);
       }
 
-#ifndef KOKKOS_ENABLE_CUDA
-      for(int simdElemIndex=0; simdElemIndex<smdata.numSimdElems; ++simdElemIndex) {
+#ifdef KOKKOS_ENABLE_CUDA
+      const int simdElemIndex = 0;
+#else
+      for(int simdElemIndex=0; simdElemIndex<smdata.numSimdElems; ++simdElemIndex)
+#endif
+      {
         extract_vector_lane(smdata.simdrhs, simdElemIndex, smdata.rhs);
         extract_vector_lane(smdata.simdlhs, simdElemIndex, smdata.lhs);
         for (int ir=0; ir < rhsSize; ++ir)
           smdata.lhs(ir, ir) /= diagRelaxFactor;
-        (*deviceCoeffApplier)(nodesPerEntity, smdata.ngpElemNodes[simdElemIndex],
+        coeffApplier(nodesPerEntity, smdata.ngpElemNodes[simdElemIndex],
                     smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
       }
-#else
-      extract_vector_lane(smdata.simdrhs, 0, smdata.rhs);
-      extract_vector_lane(smdata.simdlhs, 0, smdata.lhs);
-      for (int ir=0; ir < rhsSize; ++ir)
-        smdata.lhs(ir, ir) /= diagRelaxFactor;
-      (*deviceCoeffApplier)(nodesPerEntity, smdata.ngpElemNodes[0],
-                  smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
-#endif
     });
 }
 

src/AssembleFaceElemSolverAlgorithm.C
@@ -89,10 +89,10 @@ AssembleFaceElemSolverAlgorithm::execute()
 
   auto ngpKernels = nalu_ngp::create_ngp_view<Kernel>(activeKernels_);
   const size_t numKernels = activeKernels_.size();
+  auto coeffApplier = coeff_applier();
 
   const unsigned nodesPerEntity = nodesPerElem_;
   const unsigned numDof = numDof_;
-  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
   double diagRelaxFactor = diagRelaxFactor_;
 
   run_face_elem_algorithm(realm_.bulk_data(),
@@ -103,35 +103,25 @@ AssembleFaceElemSolverAlgorithm::execute()
 
         for (size_t i=0; i<numKernels; ++i) {
           Kernel* kernel = ngpKernels(i);
-          kernel->execute( smdata.simdlhs, smdata.simdrhs, smdata.simdFaceViews, smdata.simdElemViews, smdata.elemFaceOrdinal );
+          kernel->execute(
+            smdata.simdlhs, smdata.simdrhs, smdata.simdFaceViews,
+            smdata.simdElemViews, smdata.elemFaceOrdinal);
         }
 
 #ifdef KOKKOS_ENABLE_CUDA
-        extract_vector_lane(smdata.simdrhs, 0, smdata.rhs);
-        extract_vector_lane(smdata.simdlhs, 0, smdata.lhs);
-        for (unsigned ir=0; ir < nodesPerEntity*numDof; ++ir)
-          smdata.lhs(ir, ir) /= diagRelaxFactor;
-        (*deviceCoeffApplier)(nodesPerEntity, smdata.ngpConnectedNodes[0],
-                    smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
+        const int simdIndex = 0;
 #else
-        for(int simdIndex=0; simdIndex<smdata.numSimdFaces; ++simdIndex) {
+        for(int simdIndex=0; simdIndex<smdata.numSimdFaces; ++simdIndex)
+#endif
+        {
           extract_vector_lane(smdata.simdrhs, simdIndex, smdata.rhs);
           extract_vector_lane(smdata.simdlhs, simdIndex, smdata.lhs);
           for (unsigned ir=0; ir < nodesPerElem_*numDof; ++ir)
             smdata.lhs(ir, ir) /= diagRelaxFactor;
 
-#ifndef KOKKOS_ENABLE_CUDA
-          // TODO: Replace this with NGP version
-          if (realm_.hasOverset_)
-            reset_overset_rows(
-              realm_.meta_data(), eqSystem_->linsys_->numDof(), nodesPerEntity,
-              smdata.ngpConnectedNodes[simdIndex], smdata.rhs, smdata.lhs);
-#endif
-
-          (*deviceCoeffApplier)(nodesPerEntity, smdata.ngpConnectedNodes[simdIndex],
+          coeffApplier(nodesPerEntity, smdata.ngpConnectedNodes[simdIndex],
                       smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
         }
-#endif
     });
 }
 

src/AssembleNGPNodeSolverAlgorithm.C
@@ -85,13 +85,12 @@ AssembleNGPNodeSolverAlgorithm::execute()
     kern->setup(realm_);
 
   auto ngpKernels = nalu_ngp::create_ngp_view<NodeKernel>(nodeKernels_);
+  auto coeffApplier = coeff_applier();
 
   const auto& meta = realm_.meta_data();
   const auto& ngpMesh = realm_.ngp_mesh();
   const stk::mesh::EntityRank entityRank = stk::topology::NODE_RANK;
   const int rhsSize = rhsSize_;
-
-  CoeffApplier* deviceCoeffApplier = eqSystem_->linsys_->get_coeff_applier();
 
   const int nodesPerEntity = 1;
   const int bytes_per_team = 0;
@@ -128,15 +127,7 @@ AssembleNGPNodeSolverAlgorithm::execute()
             kernel->execute(smdata.lhs, smdata.rhs, nodeIndex);
           }
 
-#ifndef KOKKOS_ENABLE_CUDA
-          // TODO: Replace this with NGP version
-          if (realm_.hasOverset_)
-            reset_overset_rows(
-              realm_.meta_data(), eqSystem_->linsys_->numDof(), nodesPerEntity,
-              smdata.ngpNodes, smdata.rhs, smdata.lhs);
-#endif
-
-          (*deviceCoeffApplier)(
+          coeffApplier(
             nodesPerEntity, smdata.ngpNodes, smdata.scratchIds,
             smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
         });

src/SolverAlgorithm.C
@@ -12,6 +12,8 @@
 #include <LinearSystem.h>
 #include <KokkosInterface.h>
 
+#include "ngp_utils/NgpFieldUtils.h"
+
 #include <stk_mesh/base/Entity.hpp>
 
 #include <vector>
@@ -81,18 +83,83 @@ void fix_overset_rows(
 namespace sierra{
 namespace nalu{
 
-class Realm;
-class EquationSystem;
+NGPApplyCoeff::NGPApplyCoeff(EquationSystem* eqSystem)
+  : ngpMesh_(eqSystem->realm_.ngp_mesh()),
+    deviceSumInto_(eqSystem->linsys_->get_coeff_applier()),
+    nDim_(eqSystem->linsys_->numDof()),
+    hasOverset_(eqSystem->realm_.hasOverset_),
+    extractDiagonal_(eqSystem->extractDiagonal_)
+{
+  if (extractDiagonal_) {
+    diagField_ = nalu_ngp::get_ngp_field(
+      eqSystem->realm_.mesh_info(), eqSystem->get_diagonal_field()->name());
+  }
 
-//==========================================================================
-// Class Definition
-//==========================================================================
-// SolverAlgorithm - base class for algorithm with expectations of solver
-//                   contributions
-//==========================================================================
-//--------------------------------------------------------------------------
-//-------- constructor -----------------------------------------------------
-//--------------------------------------------------------------------------
+  if (hasOverset_) {
+    iblankField_ = nalu_ngp::get_ngp_field<int>(eqSystem->realm_.mesh_info(), "iblank");
+  }
+}
+
+void NGPApplyCoeff::extract_diagonal(
+  const unsigned int nEntities,
+  const ngp::Mesh::ConnectedNodes& entities,
+  SharedMemView<double**, DeviceShmem>& lhs) const
+{
+  constexpr bool forceAtomic = std::is_same<
+    sierra::nalu::DeviceSpace, Kokkos::DefaultExecutionSpace>::value;
+
+  for (unsigned i=0u; i < nEntities; ++i) {
+    auto ix = i * nDim_;
+    if (forceAtomic)
+      Kokkos::atomic_add(&diagField_.get(ngpMesh_, entities[i], 0), lhs(ix, ix));
+    else
+      diagField_.get(ngpMesh_, entities[i], 0) += lhs(ix, ix);
+  }
+}
+
+void
+NGPApplyCoeff::reset_overset_rows(
+  const unsigned int nEntities,
+  const ngp::Mesh::ConnectedNodes& entities,
+  SharedMemView<double*, DeviceShmem>& rhs,
+  SharedMemView<double**, DeviceShmem>& lhs) const
+{
+  const unsigned numRows = nEntities * nDim_;
+
+  for (unsigned in=0u; in < nEntities; ++in) {
+    const int ibl = iblankField_.get(ngpMesh_, entities[in], 0);
+    const double mask  = std::max(0.0, static_cast<double>(ibl));
+    const unsigned ix = in * nDim_;
+
+    for (unsigned d=0; d < nDim_; ++d) {
+      const unsigned ir = ix + d;
+
+      rhs(ir) *= mask;
+      for (unsigned ic=0; ic < numRows; ++ic)
+        lhs(ir, ic) *= mask;
+    }
+  }
+}
+
+void NGPApplyCoeff::operator()(
+  unsigned numMeshobjs,
+  const ngp::Mesh::ConnectedNodes& symMeshobjs,
+  const SharedMemView<int*,DeviceShmem> & scratchIds,
+  const SharedMemView<int*,DeviceShmem> & sortPermutation,
+  SharedMemView<double*,DeviceShmem> & rhs,
+  SharedMemView<double**,DeviceShmem> & lhs,
+  const char *trace_tag) const
+{
+  if (hasOverset_)
+    reset_overset_rows(numMeshobjs, symMeshobjs, rhs, lhs);
+
+  (*deviceSumInto_)(
+    numMeshobjs, symMeshobjs, scratchIds, sortPermutation, rhs, lhs, trace_tag);
+
+  if (extractDiagonal_)
+    extract_diagonal(numMeshobjs, symMeshobjs, lhs);
+}
+
 SolverAlgorithm::SolverAlgorithm(
   Realm &realm,
   stk::mesh::Part *part,

src/TpetraLinearSystem.C
@@ -1311,20 +1311,10 @@ void reset_rows(
 sierra::nalu::CoeffApplier* TpetraLinearSystem::get_coeff_applier()
 {
   if (!hostCoeffApplier) {
-    const bool extractDiagonal = equationSystem()->extractDiagonal_;
-    const unsigned diagFieldOrdinal = (extractDiagonal && equationSystem()->get_diagonal_field() !=nullptr) ?
-                      equationSystem()->get_diagonal_field()->mesh_meta_data_ordinal() : 0;
-    
-    NGPDoubleFieldType diagField;
-    if (extractDiagonal) {
-      diagField = realm_.ngp_field_manager().get_field<double>(diagFieldOrdinal);
-    }
-   
-    hostCoeffApplier.reset(new TpetraLinSysCoeffApplier(ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
-                                        ownedLocalRhs_, sharedNotOwnedLocalRhs_,
-                                        entityToLID_, entityToColLID_,
-                                        maxOwnedRowId_, maxSharedNotOwnedRowId_, numDof_,
-                                        extractDiagonal, diagField, realm_.ngp_mesh()));
+    hostCoeffApplier.reset(new TpetraLinSysCoeffApplier(
+      ownedLocalMatrix_, sharedNotOwnedLocalMatrix_, ownedLocalRhs_,
+      sharedNotOwnedLocalRhs_, entityToLID_, entityToColLID_, maxOwnedRowId_,
+      maxSharedNotOwnedRowId_, numDof_));
     deviceCoeffApplier = hostCoeffApplier->device_pointer();
   }
 
@@ -1346,13 +1336,15 @@ void TpetraLinearSystem::TpetraLinSysCoeffApplier::resetRows(unsigned numNodes,
 }
 
 KOKKOS_FUNCTION
-void TpetraLinearSystem::TpetraLinSysCoeffApplier::operator()(unsigned numEntities,
-                                                              const ngp::Mesh::ConnectedNodes& entities,
-                                                              const SharedMemView<int*,DeviceShmem> & localIds,
-                                                              const SharedMemView<int*,DeviceShmem> & sortPermutation,
-                                                              const SharedMemView<const double*,DeviceShmem> & rhs,
-                                                              const SharedMemView<const double**,DeviceShmem> & lhs,
-                                                              const char * /*trace_tag*/)
+void
+TpetraLinearSystem::TpetraLinSysCoeffApplier::operator()(
+  unsigned numEntities,
+  const ngp::Mesh::ConnectedNodes& entities,
+  const SharedMemView<int*, DeviceShmem>& localIds,
+  const SharedMemView<int*, DeviceShmem>& sortPermutation,
+  const SharedMemView<const double*, DeviceShmem>& rhs,
+  const SharedMemView<const double**, DeviceShmem>& lhs,
+  const char* /*trace_tag*/)
 {
   sum_into(
       ownedLocalMatrix_, sharedNotOwnedLocalMatrix_,
@@ -1363,20 +1355,6 @@ void TpetraLinearSystem::TpetraLinSysCoeffApplier::operator()(unsigned numEntiti
       entityToLID_, entityToColLID_,
       maxOwnedRowId_, maxSharedNotOwnedRowId_,
       numDof_);
-
-  if (extractDiagonal_) {
-    constexpr bool forceAtomic = !std::is_same<sierra::nalu::DeviceSpace, Kokkos::Serial>::value;
-    unsigned nDim = ngpMesh_.get_spatial_dimension();
-    for(unsigned i=0; i<numEntities; ++i) {
-      size_t idx = i*nDim;
-      if (forceAtomic) {
-        Kokkos::atomic_add(&diagField_.get(ngpMesh_, entities[i], 0), lhs(idx,idx));
-      }
-      else {
-        diagField_.get(ngpMesh_, entities[i], 0) += lhs(idx,idx);
-      }
-    }
-  }
 }
 
 void TpetraLinearSystem::TpetraLinSysCoeffApplier::free_device_pointer()