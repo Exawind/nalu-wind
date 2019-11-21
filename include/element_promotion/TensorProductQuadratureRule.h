29989d542d67941abb9bcbbb948203ac9ecc7ce6
Shreyas Ananthan <shreyas@umd.edu>
2019-11-21 13:46:21 -0700

    Update copyright notices on all source files and executable header

--- include/element_promotion/TensorProductQuadratureRule.h
+++ include/element_promotion/TensorProductQuadratureRule.h
@@ -1,9 +1,12 @@
-/*------------------------------------------------------------------------*/
-/*  Copyright 2014 Sandia Corporation.                                    */
-/*  This software is released under the license detailed                  */
-/*  in the file, LICENSE, which is located in the top-level Nalu          */
-/*  directory structure                                                   */
-/*------------------------------------------------------------------------*/
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+
 #ifndef TensorProductQuadratureRule_h
 #define TensorProductQuadratureRule_h
 
