c4a586dd1a14d6419446d9a79e127f05b437228c
Martinez <tonyinme@gmail.com>
2019-12-16 17:01:22 -0700

    Added example under examples/filtered_lifting_line

---
+++ examples/filtered_lifting_line/without_correction/5MW_Baseline/ServoData/CMakeFiles/DISCON.dir/DISCON.F90-pp.f90.d
@@ -0,0 +1,2 @@
+reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData/CMakeFiles/DISCON.dir/DISCON.F90-pp.f90: \
+ ../reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData/DISCON.F90
---
+++ examples/filtered_lifting_line/without_correction/5MW_Baseline/ServoData/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lmartin1/Documents/naluCode/nalu-wind/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData
+# Build directory: /Users/lmartin1/Documents/naluCode/nalu-wind/build/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
