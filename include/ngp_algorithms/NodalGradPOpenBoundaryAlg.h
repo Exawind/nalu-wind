1c7eaf208f68f8d1923199d7ee41314ce12924fe
James Overfelt <jroverf@sandia.gov>
2020-01-24 13:14:09 -0700

    Address code review of first AssembleNodalGradPOpenBoundaryAlgorithm (#488)
    * Address code review of first AssembleNodalGradPOpenBoundaryAlgorithm
    
    The changes were extensive enough that a new branch was created.
    
    This has been tested with gcc 7.2 and I still need to compile and
    test for Cuda.
    
    * Fixes from code review.
    
    * Did not mean to push this file.
    
    * Extend unit test to test zeroGrad and useShifted options.
    
    * Fix 2 processor unit test.
    
    * Update to joint copyright assertion and give credit where credit is due.
    
    * Fix unit test to run on Cuda
    
    With Alan's help, correctly synced some needed fields to device.

---
+++ include/ngp_algorithms/NodalGradPOpenBoundaryAlg.h
@@ -0,0 +1,56 @@
+// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+
+
+#ifndef NODALGRADPOPENBOUNDARYALG_H
+#define NODALGRADPOPENBOUNDARYALG_H
+
+#include<Algorithm.h>
+#include<FieldTypeDef.h>
+#include<ElemDataRequests.h>
+
+#include "stk_mesh/base/Types.hpp"
+
+namespace sierra{
+namespace nalu{
+
+template<typename AlgTraits>
+class NodalGradPOpenBoundary : public Algorithm
+{
+public:
+  NodalGradPOpenBoundary(
+    Realm &,
+    stk::mesh::Part *,
+    const bool useShifted);
+
+  virtual ~NodalGradPOpenBoundary() = default;
+
+  virtual void execute() override;
+
+  const bool useShifted_;
+  const bool zeroGrad_;
+  const bool massCorr_;
+
+  const unsigned exposedAreaVec_ {stk::mesh::InvalidOrdinal};
+  const unsigned dualNodalVol_   {stk::mesh::InvalidOrdinal};
+  const unsigned exposedPressureField_ {stk::mesh::InvalidOrdinal};
+  const unsigned pressureField_  {stk::mesh::InvalidOrdinal};
+  const unsigned gradP_          {stk::mesh::InvalidOrdinal};
+  const unsigned coordinates_    {stk::mesh::InvalidOrdinal};
+
+  MasterElement* meFC_ {nullptr};
+  MasterElement* meSCS_{nullptr};
+
+  ElemDataRequests faceData_;
+  ElemDataRequests elemData_;
+};
+
+} // namespace nalu
+} // namespace Sierra
+
+#endif
---
+++ unit_tests/UnitTestNodalGradPOpenBoundary.C
@@ -0,0 +1,102 @@
+// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+
+#include "memory"
+#include "functional"
+
+#include "UnitTestRealm.h"
+#include "UnitTestLinearSystem.h"
+#include "UnitTestUtils.h"
+
+#include "AssembleNodalGradPOpenBoundaryAlgorithm.h"
+#include "ComputeGeometryBoundaryAlgorithm.h"
+#include "SolutionOptions.h"
+
+#include <stk_mesh/base/BulkData.hpp>
+#include <stk_topology/topology.hpp>
+
+ 
+namespace sierra {
+namespace nalu{
+
+struct HelperObjectsNodalGradPOpenBoundary {
+  HelperObjectsNodalGradPOpenBoundary(
+    stk::mesh::BulkData& bulk, 
+    stk::mesh::Part* part,
+    const bool zeroGrad,
+    const bool useShifted)
+  : 
+    realmDefaultNode(unit_test_utils::get_realm_default_node()),
+    naluObj(new unit_test_utils::NaluTest(unit_test_utils::get_default_inputs())),
+    realm(naluObj->create_realm(realmDefaultNode, "multi_physics", false)),
+    NodalGradPOpenBoundaryAlg(),
+    computeGeomBoundAlg()
+  {
+    realm.metaData_ = &bulk.mesh_meta_data();
+    realm.bulkData_ = &bulk;
+    realm.solutionOptions_->activateOpenMdotCorrection_ = true;
+    realm.solutionOptions_->explicitlyZeroOpenPressureGradient_ = zeroGrad;
+    NodalGradPOpenBoundaryAlg.reset(new AssembleNodalGradPOpenBoundaryAlgorithm(realm, part, useShifted));
+    computeGeomBoundAlg.reset(new ComputeGeometryBoundaryAlgorithm(realm, part));
+  }
+
+  ~HelperObjectsNodalGradPOpenBoundary()
+  {
+    realm.metaData_ = nullptr;
+    realm.bulkData_ = nullptr;
+  }
+
+  HelperObjectsNodalGradPOpenBoundary() = delete;
+  HelperObjectsNodalGradPOpenBoundary(const HelperObjectsNodalGradPOpenBoundary&) = delete;
+
+  const YAML::Node realmDefaultNode;
+  std::unique_ptr<unit_test_utils::NaluTest> naluObj;
+  sierra::nalu::Realm& realm;
+  std::unique_ptr<AssembleNodalGradPOpenBoundaryAlgorithm> NodalGradPOpenBoundaryAlg;
+  std::unique_ptr<ComputeGeometryBoundaryAlgorithm>        computeGeomBoundAlg;
+};
+
+#ifndef KOKKOS_ENABLE_CUDA
+TEST_F(Hex8MeshWithNSOFields, nodal_grad_popen_boundary) {
+  const int np  = bulk.parallel_size();
+  const std::string meshSpec = "generated:1x1x" + std::to_string(np);
+  fill_mesh_and_initialize_test_fields(meshSpec, true);
+  stk::mesh::Part* surface1 = meta.get_part("surface_1");
+  
+  const double x = -0.125;
+  const double y =  0.25 ;
+  const stk::mesh::Selector all_local = meta.universal_part() & meta.locally_owned_part();
+  const stk::mesh::BucketVector& nodeBuckets = bulk.get_buckets(stk::topology::NODE_RANK, all_local);
+  std::function<void(bool,bool)> run_alg = [&](bool zeroGrad, bool useShifted) {
+    stk::mesh::field_fill(0.0, *dpdx);
+    HelperObjectsNodalGradPOpenBoundary helperObjs(bulk, surface1, zeroGrad, useShifted);
+    helperObjs.computeGeomBoundAlg->execute();
+    helperObjs.NodalGradPOpenBoundaryAlg->execute();
+    for (const stk::mesh::Bucket* b : nodeBuckets)
+    {
+      for (stk::mesh::Entity node : *b)
+      {
+        const double* dp = stk::mesh::field_data(*dpdx, node);
+        const double*  C = stk::mesh::field_data(*coordField, node);
+        int I[3] = {0,0,0};
+        for (int i=0; i<3; ++i) if (C[i]) I[i] = 1; // mesh boundary
+        double re[3] = {x+y*I[0], x+y*I[1], x+y*I[2]};
+        if (0 < C[2] && C[2] < np) re[2] = 0;       // mesh interior
+        for (int i=0; i<3; ++i) 
+          EXPECT_NEAR(re[i], dp[i], tol);
+      }
+    }
+  };
+  run_alg(false, false);
+  run_alg(true, false);
+  run_alg(false, true);
+  run_alg(true, true);
+}
+#endif
+}
+}
