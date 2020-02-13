163d19ce684a0a7eb15d84090393efb449b1ee12
Shreyas Ananthan <shreyas@umd.edu>
2020-02-13 14:42:14 -0700

    Attempt to fix warnings on Intel/NVCC builds for CFL/Re reducer

--- src/ngp_algorithms/CourantReAlg.C
+++ src/ngp_algorithms/CourantReAlg.C
@@ -15,6 +15,7 @@
 #include "master_element/MasterElementFactory.h"
 #include "ngp_algorithms/ViewHelper.h"
 #include "ngp_algorithms/CourantReAlgDriver.h"
+#include "ngp_algorithms/CourantReReduceHelper.h"
 #include "ngp_utils/NgpLoopUtils.h"
 #include "ngp_utils/NgpFieldOps.h"
 #include "ngp_utils/NgpReduceUtils.h"
@@ -25,88 +26,6 @@
 
 namespace sierra {
 namespace nalu {
-
-namespace {
-struct CflRe
-{
-  double max_cfl, max_re;
-
-  KOKKOS_INLINE_FUNCTION
-  CflRe()
-  {
-    max_cfl = -1.0e6;
-    max_re = -1.0e6;
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  void operator=(const CflRe& rhs)
-  {
-    max_cfl = rhs.max_cfl;
-    max_re = rhs.max_re;
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  void operator=(const volatile CflRe& rhs) volatile
-  {
-    max_cfl = rhs.max_cfl;
-    max_re = rhs.max_re;
-  }
-};
-
-template<typename Space=Kokkos::HostSpace>
-struct CflReMax
-{
-public:
-  typedef CflReMax reducer;
-  typedef CflRe value_type;
-  typedef Kokkos::View<value_type, Space> result_view_type;
-
-private:
-  result_view_type value;
-  bool references_scalar_v;
-
-public:
-  KOKKOS_INLINE_FUNCTION
-  CflReMax(value_type& value_): value(&value_),references_scalar_v(true) {}
-
-  KOKKOS_INLINE_FUNCTION
-  CflReMax(const result_view_type& value_): value(value_),references_scalar_v(false) {}
-
-  //Required
-  KOKKOS_INLINE_FUNCTION
-  void join(value_type& dest, const value_type& src)  const {
-    if (dest.max_cfl < src.max_cfl) dest.max_cfl = src.max_cfl;
-    if (dest.max_re < src.max_re) dest.max_re = src.max_re;
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  void join(volatile value_type& dest, const volatile value_type& src) const {
-    if (dest.max_cfl < src.max_cfl) dest.max_cfl = src.max_cfl;
-    if (dest.max_re < src.max_re) dest.max_re = src.max_re;
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  void init( value_type& val)  const {
-    val.max_cfl = Kokkos::reduction_identity<double>::max();;
-    val.max_re = Kokkos::reduction_identity<double>::max();
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  value_type& reference() const {
-    return *value.data();
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  result_view_type view() const {
-    return value;
-  }
-
-  KOKKOS_INLINE_FUNCTION
-  bool references_scalar() const {
-    return references_scalar_v;
-  }
-};
-}
 
 template<typename AlgTraits>
 CourantReAlg<AlgTraits>::CourantReAlg(
