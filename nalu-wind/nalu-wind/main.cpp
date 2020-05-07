aed868f29f969a799b7969654bb97decc45a22b4
mchurchf <matt.churchfield@nrel.gov>
2020-05-07 09:02:23 -0400

    First commit of a new branch for getting upgraded ABL wall BC into mainline nalu-wind.

---
+++ nalu-wind/nalu-wind/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nalu-wind
+//
+//  Created by Churchfield, Matthew on 5/1/20.
+//  Copyright © 2020 National Renewable Energy Laboratory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
