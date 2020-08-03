dbdb4d58fde49d26d2b431c2be8e0d119faae51c
Paul Mullowney <pmullown@nrel.gov>
2020-08-03 07:39:12 -0600

    fixing this commented out test

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -238,7 +238,7 @@ else(NOT ENABLE_CUDA)
   #=============================================================================
   add_test_r(ablNeutralNGPTrilinos 2)
   add_test_r(conduction_p4 2)
-  #add_test_r(airfoilRANSEdgeTrilinos 1)
+  add_test_r(airfoilRANSEdgeTrilinos 1)
 
   if(ENABLE_HYPRE)
     add_test_r(airfoilRANSEdgeNGP 2)
