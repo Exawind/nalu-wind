5230e7d4f698abc99007349bc8cfed06eb12949d
Robert C. Knaus <rcknaus@sandia.gov>
2020-08-24 15:06:35 -0600

    remove include

--- unit_tests/matrix_free/UnitTestMatrixFreeSolver.C
+++ unit_tests/matrix_free/UnitTestMatrixFreeSolver.C
@@ -10,7 +10,6 @@
 #include "matrix_free/MatrixFreeSolver.h"
 #include "matrix_free/ConductionFields.h"
 #include "matrix_free/ConductionOperator.h"
-#include "matrix_free/StkEntityToRowMap.h"
 #include "matrix_free/StkSimdConnectivityMap.h"
 #include "matrix_free/StkToTpetraMap.h"
 #include "matrix_free/StkToTpetraLocalIndices.h"
