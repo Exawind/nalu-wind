Exawind/nalu-wind

commit 5fe35de5f2e4672805431ebfaf4b46bd99d25ecf

Add the IDDES Model (#725)
* Implementation of Strelets numerical blending of upwind and central difference

* Updating momentum edge solver to use the new blending field alpha_upw

* Changing initialization of alpha_upw and use with SST_DES

* Add IDDES capability

* Add IDDES+ABL capability

* Fix typo and add blending of initial condition between RANS and LES

* Fix errors while rebasing to latest master

* Fix some typos

* Compile off latest master

* Revert changes to MomentumEdgeSolverAlg.C

* Add original functions back into low mach eqs

* Lay ground work for moving peclet computation to utility

* Create Edge preprocessor for peclet comp

* Update unit test due to peclet extraction

* Move Strelets comp to an alg

Make Strelets computation an algorithm that
swaps out the standard peclet implementation

Also convert Strelets computation to be edge based
instead of node based

* Move error check to Strelets

* Restore original functionality to open BC

* Add peclet factor registration

* Update mesh snapshot

* Update copy rights and add a unit test

Unit test still WIP

* Response to reviews

* Small refactor of conditionals

* Move IDDES_ABL stuff out of lowmacheqs

* Remove default constructors

* Get unit test alg to run

* Finish unit test for Strelets

* Add regression test. Not ready to merge yet

* IDDES Reg test running

Seeing similar convergence to SSTPeriodicHillEdge and fields look
the same after the first 10 timesteps

* Remove IDDESABL

Removing for now since I'm not testing it and to make the PR smaller
We can always add it back via a separate PR:

* Remove intentional failing of Strelets unit test

Co-authored-by: Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>

src/LowMachEquationSystem.C
@@ -109,6 +109,8 @@
 #include <edge_kernels/MomentumOpenEdgeKernel.h>
 #include <edge_kernels/MomentumABLWallShearStressEdgeKernel.h>
 #include <edge_kernels/MomentumSymmetryEdgeKernel.h>
+#include <edge_kernels/MomentumEdgePecletAlg.h>
+#include <edge_kernels/StreletsUpwindEdgeAlg.h>
 
 // node kernels
 #include "node_kernels/NodeKernelUtils.h"
@@ -146,6 +148,8 @@
 #include "ngp_utils/NgpFieldBLAS.h"
 #include "ngp_utils/NgpFieldUtils.h"
 #include "stk_mesh/base/NgpFieldParallel.hpp"
+#include "ngp_utils/NgpTypes.h"
+
 
 // nso
 #include <nso/MomentumNSOElemKernel.h>
@@ -217,6 +221,7 @@
 #include <stk_util/util/SortAndUnique.hpp>
 
 // stk_mesh/base/fem
+#include "stk_mesh/base/Types.hpp"
 #include <stk_mesh/base/BulkData.hpp>
 #include <stk_mesh/base/Field.hpp>
 #include <stk_mesh/base/FieldParallel.hpp>
@@ -374,6 +379,7 @@ LowMachEquationSystem::register_nodal_fields(
                                stk::topology::NODE_RANK);
     copyStateAlg_.push_back(theCopyAlgDlNdVol);
   }
+
 }
 
 //--------------------------------------------------------------------------
@@ -498,10 +504,10 @@ LowMachEquationSystem::register_open_bc(
                                                  "open_mass_flow_rate"));
   stk::mesh::put_field_on_mesh(*mdotBip, *part, numScsBip, nullptr);
 
-  auto& dynPress 
+  auto& dynPress
     = metaData.declare_field<GenericFieldType>(static_cast<stk::topology::rank_t>(metaData.side_rank()),
                                                  "dynamic_pressure");
-  std::vector<double> ic(numScsBip, 0);                                              
+  std::vector<double> ic(numScsBip, 0);
   stk::mesh::put_field_on_mesh(dynPress, *part, numScsBip, ic.data());
 }
 
@@ -685,7 +691,6 @@ LowMachEquationSystem::solve_and_update()
     continuityEqSys_->compute_projected_nodal_gradient();
     timeA = NaluEnv::self().nalu_time();
     continuityEqSys_->mdotAlgDriver_->execute();
-
     timeB = NaluEnv::self().nalu_time();
     continuityEqSys_->timerMisc_ += (timeB-timeA);
 
@@ -706,6 +711,7 @@ LowMachEquationSystem::solve_and_update()
 
     for (int oi=0; oi < momentumEqSys_->numOversetIters_; ++oi) {
       momentumEqSys_->dynPressAlgDriver_.execute();
+      momentumEqSys_->pecletAlg_->execute();
       momentumEqSys_->assemble_and_solve(momentumEqSys_->uTmp_);
 
       timeA = NaluEnv::self().nalu_time();
@@ -1034,6 +1040,7 @@ MomentumEquationSystem::initial_work()
     const double timeA = NaluEnv::self().nalu_time();
     compute_wall_function_params();
     compute_turbulence_parameters();
+    pecletAlg_->execute();
     cflReAlgDriver_.execute();
 
     const double timeB = NaluEnv::self().nalu_time();
@@ -1105,7 +1112,7 @@ MomentumEquationSystem::register_nodal_fields(
   }
 
   Udiag_ = &(meta_data.declare_field<ScalarFieldType>(
-               stk::topology::NODE_RANK, "momentum_diag"));
+    stk::topology::NODE_RANK, "momentum_diag"));
   stk::mesh::put_field_on_mesh(*Udiag_, *part, nullptr);
   realm_.augment_restart_variable_list("momentum_diag");
 
@@ -1141,7 +1148,6 @@ MomentumEquationSystem::register_nodal_fields(
     stk::mesh::put_field_on_mesh(*actuatorSourceLHS, *part, nDim, nullptr);
     stk::mesh::put_field_on_mesh(*g, *part, nullptr);
   }
-
 }
 
 //--------------------------------------------------------------------------
@@ -1160,6 +1166,10 @@ void
 MomentumEquationSystem::register_edge_fields(
   stk::mesh::Part * part)
 {
+  ScalarFieldType* pecletFactor =
+    &(realm_.meta_data().declare_field<ScalarFieldType>(
+      stk::topology::EDGE_RANK, "peclet_factor"));
+  stk::mesh::put_field_on_mesh(*pecletFactor, *part, nullptr);
   if (realm_.solutionOptions_->turbulenceModel_ == SST_TAMS)
     TAMSAlgDriver_->register_edge_fields(part);
 }
@@ -1193,6 +1203,8 @@ MomentumEquationSystem::register_interior_algorithm(
         edgeNodalGradient_);
   }
 
+  const auto theTurbModel = realm_.solutionOptions_->turbulenceModel_;
+
   // solver; interior contribution (advection + diffusion) [possible CMM time]
   if ( !realm_.solutionOptions_->useConsolidatedSolverAlg_ ) {
     std::map<AlgorithmType, SolverAlgorithm *>::iterator itsi
@@ -1201,10 +1213,15 @@ MomentumEquationSystem::register_interior_algorithm(
       SolverAlgorithm *theSolverAlg = NULL;
       if ( realm_.realmUsesEdges_ ) {
         theSolverAlg = new MomentumEdgeSolverAlg(realm_, part, this);
-        if (realm_.solutionOptions_->turbulenceModel_ == SST_TAMS) {
+        if (theTurbModel == SST_TAMS) {
           SolverAlgorithm *theSolverSrcAlg = NULL;
           theSolverSrcAlg = new AssembleTAMSEdgeKernelAlg(realm_, part, this);
           solverAlgDriver_->solverAlgMap_[SRC] = theSolverSrcAlg;
+        }
+        if (realm_.is_turbulent() && theTurbModel == SST_IDDES) {
+          pecletAlg_.reset(new StreletsUpwindEdgeAlg(realm_, part));
+        } else {
+          pecletAlg_.reset(new MomentumEdgePecletAlg(realm_, part, this));
         }
       }
       else {
@@ -1545,6 +1562,8 @@ MomentumEquationSystem::register_interior_algorithm(
 
       case SST:
       case SST_DES:
+      case SST_IDDES:
+
         tviscAlg_.reset(new TurbViscSSTAlg(realm_, part, tvisc_));
         break;
 
@@ -1767,7 +1786,7 @@ MomentumEquationSystem::register_open_bc(
           (partTopo, elemTopo, *this, activeKernels, "momentum_open",
            realm_.meta_data(), realm_.solutionOptions_,
            realm_.is_turbulent() ? evisc_ : visc_,
-           faceElemSolverAlg->faceDataNeeded_, faceElemSolverAlg->elemDataNeeded_, 
+           faceElemSolverAlg->faceDataNeeded_, faceElemSolverAlg->elemDataNeeded_,
            userData.entrainMethod_);
       }
       else {
@@ -2414,6 +2433,7 @@ MomentumEquationSystem::initialize()
     const double dt = realm_.get_time_step();
     const double gamma1 = realm_.get_gamma1();
     stk::mesh::field_fill(gamma1/dt, *Udiag_);
+
     Udiag_->modify_on_host();
   }
 }

.gitignore
@@ -13,3 +13,4 @@ tags
 .vscode/
 compile_commands.json
 .clangd
+spack*

include/Enums.h
@@ -166,6 +166,7 @@ enum TurbulenceModel {
   SST = 4,
   SST_DES = 5,
   SST_TAMS = 6,
+  SST_IDDES = 7,
   TurbulenceModel_END
 };  
 
@@ -177,7 +178,8 @@ static const std::string TurbulenceModelNames[] = {
   "wale",
   "sst",
   "sst_des",
-  "sst_tams"};
+  "sst_tams",
+  "sst_iddes"};
 
 enum TurbulenceModelConstant {
   TM_cMu = 0,
@@ -213,7 +215,20 @@ enum TurbulenceModelConstant {
   TM_forBlKol = 30,
   TM_forFac = 31,
   TM_v2cMu = 32,
-  TM_END = 33
+  TM_sigmaMax = 33,
+  TM_ch1 = 34,
+  TM_ch2 = 35,
+  TM_ch3 = 36,
+  TM_tau_des = 37,
+  TM_iddes_Cw = 38,
+  TM_iddes_Cdt1 = 39,
+  TM_iddes_Cdt2 = 40,
+  TM_iddes_Cl = 41,
+  TM_iddes_Ct = 42,
+  TM_abl_bndtw = 43,
+  TM_abl_deltandtw = 44,
+  TM_abl_sigma = 45,
+  TM_END = 46
 };
 
 static const std::string TurbulenceModelConstantNames[] = {
@@ -250,6 +265,19 @@ static const std::string TurbulenceModelConstantNames[] = {
   "forcingBlKol",
   "forcingFactor",
   "v2cMu",
+  "sigmaMax",
+  "ch1",
+  "ch2",
+  "ch3",
+  "tau_des",
+  "iddes_Cw",
+  "iddes_Cdt1",
+  "iddes_Cdt2",
+  "iddes_Cl",
+  "iddes_Ct",
+  "abl_bndtw",
+  "abl_deltandtw",
+  "abl_sigma",
   "END"};
 
 enum ActuatorType {

include/LowMachEquationSystem.h
@@ -221,6 +221,7 @@ public:
   NgpAlgDriver dynPressAlgDriver_;
   std::unique_ptr<EffDiffFluxCoeffAlg> diffFluxCoeffAlg_{nullptr};
   std::unique_ptr<Algorithm> tviscAlg_{nullptr};
+  std::unique_ptr<Algorithm> pecletAlg_{nullptr};
 
   CourantReAlgDriver cflReAlgDriver_;
   std::unique_ptr<TAMSAlgDriver> TAMSAlgDriver_{nullptr};

include/TurbKineticEnergyEquationSystem.h
@@ -89,6 +89,8 @@ public:
   void compute_projected_nodal_gradient();
 
   void post_external_data_transfer_work();
+  static
+  bool check_for_valid_turblence_model(TurbulenceModel turbModel);
   
   const bool managePNG_;
 

include/edge_kernels/MomentumEdgePecletAlg.h
@@ -1,0 +1,48 @@
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+
+#ifndef MOMENTUMEDGEPECLETKERNEL_H_
+#define MOMENTUMEDGEPECLETKERNEL_H_
+
+#include <Algorithm.h>
+#include "stk_mesh/base/Types.hpp"
+#include "PecletFunction.h"
+
+namespace sierra{
+namespace nalu{
+
+class Realm;
+class EquationSystem;
+
+class MomentumEdgePecletAlg: public Algorithm{
+public:
+  using DblType = double;
+
+  MomentumEdgePecletAlg(Realm& , stk::mesh::Part*, EquationSystem*);
+  virtual ~MomentumEdgePecletAlg() = default;
+  void execute() override;
+
+  
+private:
+  unsigned pecletFactor_ {stk::mesh::InvalidOrdinal};
+  unsigned density_ {stk::mesh::InvalidOrdinal};
+  unsigned viscosity_ {stk::mesh::InvalidOrdinal};
+  unsigned coordinates_ {stk::mesh::InvalidOrdinal};
+  unsigned vrtm_ {stk::mesh::InvalidOrdinal};
+  unsigned edgeAreaVec_ {stk::mesh::InvalidOrdinal};
+  const double eps_{1.0e-16};
+  const int nDim_;
+  PecletFunction<DblType>* pecletFunction_{nullptr};
+};
+
+}
+}
+
+
+#endif /* MOMENTUMEDGEPECLETKERNEL_H_ */

include/edge_kernels/MomentumEdgeSolverAlg.h
@@ -33,13 +33,11 @@ private:
   unsigned coordinates_ {stk::mesh::InvalidOrdinal};
   unsigned velocityRTM_ {stk::mesh::InvalidOrdinal};
   unsigned velocity_ {stk::mesh::InvalidOrdinal};
-  unsigned density_ {stk::mesh::InvalidOrdinal};
   unsigned dudx_ {stk::mesh::InvalidOrdinal};
   unsigned edgeAreaVec_ {stk::mesh::InvalidOrdinal};
   unsigned massFlowRate_ {stk::mesh::InvalidOrdinal};
   unsigned viscosity_ {stk::mesh::InvalidOrdinal};
-
-  PecletFunction<AssembleEdgeSolverAlgorithm::DblType>* pecletFunction_{nullptr};
+  unsigned pecletFactor_ {stk::mesh::InvalidOrdinal};
 };
 
 }  // nalu

include/edge_kernels/MomentumOpenEdgeKernel.h
@@ -55,6 +55,7 @@ public:
     int);
 
 private:
+
   const unsigned coordinates_{stk::mesh::InvalidOrdinal};
   const unsigned dudx_{stk::mesh::InvalidOrdinal};
   const unsigned exposedAreaVec_{stk::mesh::InvalidOrdinal};
@@ -62,10 +63,12 @@ private:
   const unsigned velocityBc_{stk::mesh::InvalidOrdinal};
   const unsigned velocityNp1_{stk::mesh::InvalidOrdinal};
   const unsigned viscosity_{stk::mesh::InvalidOrdinal};
+  const unsigned alpha_upw_{stk::mesh::InvalidOrdinal};
 
   const double includeDivU_;
   const double nfEntrain_;
   const EntrainmentMethod entrain_;
+  const TurbulenceModel turbModel_; 
 
   MasterElement* meFC_{nullptr};
   MasterElement* meSCS_{nullptr};

commit a305b1501a6dd283b59ca5ee9b035ec73b756b2e

Fix segfaults introduced in #725

src/LowMachEquationSystem.C
@@ -711,7 +711,7 @@ LowMachEquationSystem::solve_and_update()
 
     for (int oi=0; oi < momentumEqSys_->numOversetIters_; ++oi) {
       momentumEqSys_->dynPressAlgDriver_.execute();
-      momentumEqSys_->pecletAlg_->execute();
+      if (momentumEqSys_->pecletAlg_) momentumEqSys_->pecletAlg_->execute();
       momentumEqSys_->assemble_and_solve(momentumEqSys_->uTmp_);
 
       timeA = NaluEnv::self().nalu_time();
@@ -1040,7 +1040,7 @@ MomentumEquationSystem::initial_work()
     const double timeA = NaluEnv::self().nalu_time();
     compute_wall_function_params();
     compute_turbulence_parameters();
-    pecletAlg_->execute();
+    if (pecletAlg_) pecletAlg_->execute();
     cflReAlgDriver_.execute();
 
     const double timeB = NaluEnv::self().nalu_time();