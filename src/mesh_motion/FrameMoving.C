3f95ee812b8a9b38d24cf87b5b5d0fbcd3af2291
ashesh2512 <ashesh.sharma@nrel.gov>
2020-11-08 14:48:03 -0700

    use already existing wrapper of nalu_ngp::run_entity_algorithm

--- src/mesh_motion/FrameMoving.C
+++ src/mesh_motion/FrameMoving.C
@@ -2,9 +2,8 @@
 #include "mesh_motion/FrameMoving.h"
 
 #include "FieldTypeDef.h"
-
-// stk_mesh/base/fem
-#include "stk_mesh/base/GetNgpField.hpp"
+#include "ngp_utils/NgpLoopUtils.h"
+#include "ngp_utils/NgpTypes.h"
 
 #include <cassert>
 
@@ -24,6 +23,10 @@ void FrameMoving::update_coordinates_velocity(const double time)
   const auto& ngpMesh = bulk_.get_updated_ngp_mesh();
   const stk::mesh::EntityRank entityRank = stk::topology::NODE_RANK;
 
+  // get the parts in the current motion frame
+  stk::mesh::Selector sel = stk::mesh::selectUnion(partVec_) &
+      (meta_.locally_owned_part() | meta_.globally_shared_part());
+
   // get the field from the NGP mesh
   stk::mesh::NgpField<double> modelCoords = stk::mesh::get_updated_ngp_field<double>(
     *meta_.get_field<VectorFieldType>(entityRank, "coordinates"));
@@ -34,79 +37,65 @@ void FrameMoving::update_coordinates_velocity(const double time)
   stk::mesh::NgpField<double> meshVelocity = stk::mesh::get_updated_ngp_field<double>(
     *meta_.get_field<VectorFieldType>(entityRank, "mesh_velocity"));
 
-  // get the parts in the current motion frame
-  stk::mesh::Selector sel = stk::mesh::selectUnion(partVec_) &
-      (meta_.locally_owned_part() | meta_.globally_shared_part());
-  const auto& buckets = stk::mesh::get_bucket_ids(bulk_, entityRank, sel);
-
   // always reset velocity field
-//  stk::mesh::field_fill(0.0, meshVelocity, sel);
+  nalu_ngp::run_entity_algorithm(
+    "FrameMoving_reset_velocity",
+    ngpMesh, entityRank, sel,
+    KOKKOS_LAMBDA(const nalu_ngp::NGPMeshTraits<stk::mesh::NgpMesh>::MeshIndex& mi) {
+    for (int d = 0; d < nDim; ++d)
+      meshVelocity.get(mi,d) = 0.0;
+  });
 
-  // memory requirements for NGP loop
-  const int bytes_per_team = 0;
-  const int bytes_per_thread = 0;
-  auto team_exec = get_device_team_policy(buckets.size(), bytes_per_team, bytes_per_thread);
+  // NGP for loop to update coordinates and velocity
+  nalu_ngp::run_entity_algorithm(
+    "FrameMoving_update_coordinates_velocity",
+    ngpMesh, entityRank, sel,
+    KOKKOS_LAMBDA(const nalu_ngp::NGPMeshTraits<stk::mesh::NgpMesh>::MeshIndex& mi) {
 
-  // NGP for loop
-  Kokkos::parallel_for(
-    team_exec, KOKKOS_LAMBDA(const DeviceTeamHandleType& team) {
-    auto bktId = buckets.device_get(team.league_rank());
-    auto& b = ngpMesh.get_bucket(entityRank, bktId);
+    // temporary current and model coords for a generic 2D and 3D implementation
+    double mX[3] = {0.0,0.0,0.0};
+    double cX[3] = {0.0,0.0,0.0};
 
-    const size_t bktLen = b.size();
-    Kokkos::parallel_for(
-      Kokkos::TeamThreadRange(team, bktLen),
-      [&](const size_t& bktIndex) {
-        auto node = b[bktIndex];
-        const auto nInd = ngpMesh.fast_mesh_index(node);
+    // copy over model coordinates and reset velocity
+    for (int d = 0; d < nDim; ++d)
+      mX[d] = modelCoords.get(mi,d);
 
-        // temporary current and model coords for a generic 2D and 3D implementation
-        double mX[3] = {0.0,0.0,0.0};
-        double cX[3] = {0.0,0.0,0.0};
+    // all frame motions are based off of the reference frame
+    NgpMotion::TransMatType comp_trans_mat = NgpMotion::identity_mat();
+    for (size_t i=0; i < numKernels; ++i) {
+      NgpMotion* kernel = ngpKernels(i);
+      // build and get transformation matrix
+      kernel->build_transformation(time,mX);
+      // composite addition of motions in current group
+      comp_trans_mat = kernel->add_motion(kernel->get_trans_mat(),comp_trans_mat);
+    }
 
-        // copy over model coordinates and reset velocity
-        for (int d = 0; d < nDim; ++d) {
-          mX[d] = modelCoords.get(nInd,d);
-          meshVelocity.get(nInd,d) = 0.0;
-        }
+    // perform matrix multiplication between transformation matrix
+    // and old coordinates to obtain current coordinates
+    for (int d = 0; d < nDim; ++d) {
+      currCoords.get(mi,d) = comp_trans_mat[d][0]*mX[0]
+                              +comp_trans_mat[d][1]*mX[1]
+                              +comp_trans_mat[d][2]*mX[2]
+                              +comp_trans_mat[d][3];
 
-        // all frame motions are based off of the reference frame
-        NgpMotion::TransMatType comp_trans_mat = NgpMotion::identity_mat();
-        for (size_t i=0; i < numKernels; ++i) {
-          NgpMotion* kernel = ngpKernels(i);
-          // build and get transformation matrix
-          kernel->build_transformation(time,mX);
-          // composite addition of motions in current group
-          comp_trans_mat = kernel->add_motion(kernel->get_trans_mat(),comp_trans_mat);
-        }
+      displacement.get(mi,d) = currCoords.get(mi,d) - modelCoords.get(mi,d);
+    } // end for loop - d index
 
-        // perform matrix multiplication between transformation matrix
-        // and old coordinates to obtain current coordinates
-        for (int d = 0; d < nDim; ++d) {
-          currCoords.get(nInd,d) = comp_trans_mat[d][0]*mX[0]
-                                  +comp_trans_mat[d][1]*mX[1]
-                                  +comp_trans_mat[d][2]*mX[2]
-                                  +comp_trans_mat[d][3];
+    // copy over current coordinates
+    for (int d = 0; d < nDim; ++d)
+      cX[d] = currCoords.get(mi,d);
 
-          displacement.get(nInd,d) = currCoords.get(nInd,d) - modelCoords.get(nInd,d);
-        } // end for loop - d index
+    // compute velocity vector on current node resulting from all
+    // motions in current motion frame
+    for (size_t i=0; i < numKernels; ++i) {
+      NgpMotion* kernel = ngpKernels(i);
+      // build and get transformation matrix
+      NgpMotion::ThreeDVecType mm_vel = kernel->compute_velocity(time,comp_trans_mat,mX,cX);
 
-        // copy over current coordinates
-        for (int d = 0; d < nDim; ++d)
-          cX[d] = currCoords.get(nInd,d);
-
-        // compute velocity vector on current node resulting from all
-        // motions in current motion frame
-        for (size_t i=0; i < numKernels; ++i) {
-          NgpMotion* kernel = ngpKernels(i);
-          // build and get transformation matrix
-          NgpMotion::ThreeDVecType mm_vel = kernel->compute_velocity(time,comp_trans_mat,mX,cX);
-
-          for (int d = 0; d < nDim; ++d)
-            meshVelocity.get(nInd,d) += mm_vel[d];
-        } // end for loop - mm
-    });
-  }); // end for loop - bkts
+      for (int d = 0; d < nDim; ++d)
+        meshVelocity.get(mi,d) += mm_vel[d];
+    } // end for loop - mm
+  }); // end NGP for loop
 }
 
 void FrameMoving::post_compute_geometry()
--- src/mesh_motion/FrameReference.C
+++ src/mesh_motion/FrameReference.C
@@ -2,9 +2,8 @@
 #include "mesh_motion/FrameReference.h"
 
 #include "FieldTypeDef.h"
-
-// stk_mesh/base/fem
-#include "stk_mesh/base/GetNgpField.hpp"
+#include "ngp_utils/NgpLoopUtils.h"
+#include "ngp_utils/NgpTypes.h"
 
 #include <cassert>
 
@@ -31,54 +30,40 @@ void FrameReference::update_coordinates(const double time)
   // get the parts in the current motion frame
   stk::mesh::Selector sel = stk::mesh::selectUnion(partVec_) &
       (meta_.locally_owned_part() | meta_.globally_shared_part());
-  const auto& buckets = stk::mesh::get_bucket_ids(bulk_, entityRank, sel);
 
-  // memory requirements for NGP loop
-  const int bytes_per_team = 0;
-  const int bytes_per_thread = 0;
-  auto team_exec = get_device_team_policy(buckets.size(), bytes_per_team, bytes_per_thread);
+  // NGP for loop to update coordinates
+  nalu_ngp::run_entity_algorithm(
+    "FrameReference_update_coordinates",
+    ngpMesh, entityRank, sel,
+    KOKKOS_LAMBDA(const nalu_ngp::NGPMeshTraits<stk::mesh::NgpMesh>::MeshIndex& mi) {
 
-  // NGP for loop
-  Kokkos::parallel_for(
-    team_exec, KOKKOS_LAMBDA(const DeviceTeamHandleType& team) {
-    auto bktId = buckets.device_get(team.league_rank());
-    auto& b = ngpMesh.get_bucket(entityRank, bktId);
+    // temporary model coords for a generic 2D and 3D implementation
+    double mX[3] = {0.0,0.0,0.0};
 
-    const size_t bktLen = b.size();
-    Kokkos::parallel_for(
-      Kokkos::TeamThreadRange(team, bktLen),
-      [&](const size_t& bktIndex) {
-        auto node = b[bktIndex];
-        const auto nInd = ngpMesh.fast_mesh_index(node);
+    // copy over model coordinates
+    for (int d = 0; d < nDim; ++d)
+      mX[d] = modelCoords.get(mi,d);
 
-        // temporary model coords for a generic 2D and 3D implementation
-        double mX[3] = {0.0,0.0,0.0};
+    // all frame motions are based off of the reference frame
+    NgpMotion::TransMatType comp_trans_mat = NgpMotion::identity_mat();
 
-        // copy over model coordinates
-        for (int d = 0; d < nDim; ++d)
-          mX[d] = modelCoords.get(nInd,d);
+    for (size_t i=0; i < numKernels; ++i) {
+      NgpMotion* kernel = ngpKernels(i);
+      // build and get transformation matrix
+      kernel->build_transformation(time,mX);
+      // composite addition of motions in current group
+      comp_trans_mat = kernel->add_motion(kernel->get_trans_mat(),comp_trans_mat);
+    }
 
-        // all frame motions are based off of the reference frame
-        NgpMotion::TransMatType comp_trans_mat = NgpMotion::identity_mat();
-
-        for (size_t i=0; i < numKernels; ++i) {
-          NgpMotion* kernel = ngpKernels(i);
-          // build and get transformation matrix
-          kernel->build_transformation(time,mX);
-          // composite addition of motions in current group
-          comp_trans_mat = kernel->add_motion(kernel->get_trans_mat(),comp_trans_mat);
-        }
-
-        // perform matrix multiplication between transformation matrix
-        // and old coordinates to obtain current coordinates
-        for (int d = 0; d < nDim; ++d) {
-          modelCoords.get(nInd,d) = comp_trans_mat[d][0]*mX[0]
-                                   +comp_trans_mat[d][1]*mX[1]
-                                   +comp_trans_mat[d][2]*mX[2]
-                                   +comp_trans_mat[d][3];
-        } // end for loop - d index
-    });
-  }); // end for loop - bkts
+    // perform matrix multiplication between transformation matrix
+    // and old coordinates to obtain current coordinates
+    for (int d = 0; d < nDim; ++d) {
+      modelCoords.get(mi,d) = comp_trans_mat[d][0]*mX[0]
+                             +comp_trans_mat[d][1]*mX[1]
+                             +comp_trans_mat[d][2]*mX[2]
+                             +comp_trans_mat[d][3];
+    } // end for loop - d index
+  }); // end NGP for loop
 }
 
 } // nalu
