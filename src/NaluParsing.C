0a2c14c4321559da7af1d41ae85439a87e3f4061
psakiev <psakiev@sandia.gov>
2020-11-13 06:20:24 -0700

    Fix Tioga breakage

--- src/NaluParsing.C
+++ src/NaluParsing.C
@@ -171,7 +171,7 @@ namespace sierra
       {
         case OversetBoundaryConditionData::TPL_TIOGA:
 #ifdef NALU_USES_TIOGA
-          oversetBC.userData_.oversetBlocks_ = oversetUserData;
+          oversetBC.userData_.oversetBlocks_ = node["overset_user_data"];
           break;
 #else
           throw std::runtime_error(
