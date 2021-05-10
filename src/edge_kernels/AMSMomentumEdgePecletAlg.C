1e694101bd815a0e4706b60c3d4fff66efd7a729
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-05-10 10:56:08 -0600

    Updating AMS peclet alg

--- src/edge_kernels/AMSMomentumEdgePecletAlg.C
+++ src/edge_kernels/AMSMomentumEdgePecletAlg.C
@@ -19,6 +19,8 @@
 #include <ngp_utils/NgpLoopUtils.h>
 #include <stk_mesh/base/FieldParallel.hpp>
 #include <stk_mesh/base/FieldBLAS.hpp>
+#include <utils/AMSUtils.h>
+#include <EigenDecomposition.h>
 
 namespace sierra {
 namespace nalu {
@@ -31,12 +33,21 @@ AMSMomentumEdgePecletAlg::AMSMomentumEdgePecletAlg(
     density_(get_field_ordinal(realm.meta_data(), "density")),
     viscosity_(get_field_ordinal(realm.meta_data(), "viscosity")),
     tvisc_(get_field_ordinal(realm.meta_data(), "turbulent_viscosity")),
+    beta_(get_field_ordinal(realm.meta_data(), "k_ratio")),
+    tke_(get_field_ordinal(realm.meta_data(), "turbulent_ke")),
+    sdr_(get_field_ordinal(realm.meta_data(), "specific_dissipation_rate")),
+    nodalMij_(get_field_ordinal(realm.meta_data(), "metric_tensor")),
+    avgResAdeq_(
+      get_field_ordinal(realm.meta_data(), "avg_res_adequacy_parameter")),
+    avgVelocity_(get_field_ordinal(realm.meta_data(), "average_velocity")),
     coordinates_(get_field_ordinal(
       realm.meta_data(), realm.solutionOptions_->get_coordinates_name())),
     vrtm_(get_field_ordinal(
       realm.meta_data(), realm.does_mesh_move() ? "velocity_rtm" : "velocity")),
     edgeAreaVec_(get_field_ordinal(
       realm.meta_data(), "edge_area_vector", stk::topology::EDGE_RANK)),
+    betaStar_(realm.get_turb_model_constant(TM_betaStar)),
+    CMdeg_(realm.get_turb_model_constant(TM_CMdeg)),
     nDim_(realm.meta_data().spatial_dimension())
 {
   const std::string dofName = "velocity";
@@ -54,6 +65,12 @@ AMSMomentumEdgePecletAlg::execute()
   const auto density = fieldMgr.get_field<double>(density_);
   const auto viscosity = fieldMgr.get_field<double>(viscosity_);
   const auto tvisc = fieldMgr.get_field<double>(tvisc_);
+  const auto beta = fieldMgr.get_field<double>(beta_);
+  const auto tke = fieldMgr.get_field<double>(tke_);
+  const auto sdr = fieldMgr.get_field<double>(sdr_);
+  const auto avgResAdeq = fieldMgr.get_field<double>(avgResAdeq_);
+  const auto nodalMij = fieldMgr.get_field<double>(nodalMij_);
+  const auto avgu = fieldMgr.get_field<double>(avgVelocity_);
   const auto coordinates = fieldMgr.get_field<double>(coordinates_);
   const auto vrtm = fieldMgr.get_field<double>(vrtm_);
   const auto edgeAreaVec = fieldMgr.get_field<double>(edgeAreaVec_);
@@ -72,7 +89,7 @@ AMSMomentumEdgePecletAlg::execute()
     "compute_peclet_factor", ngpMesh, sel,
     KOKKOS_LAMBDA(const EntityInfoType& eInfo) {
       NALU_ALIGNED DblType av[nalu_ngp::NDimMax];
-      DblType asq{0.0}, axdx{0.0}, udotx{0.0};
+      DblType asq{0.0}, axdx{0.0}, udotx{0.0}, avgudotx{0.0}, fluctudotx{0.0};
 
       const auto edge = eInfo.meshIdx;
       for (int d = 0; d < ndim; d++) {
@@ -81,26 +98,90 @@ AMSMomentumEdgePecletAlg::execute()
       const auto& nodes = eInfo.entityNodes;
       const auto nodeL = ngpMesh.fast_mesh_index(nodes[0]);
       const auto nodeR = ngpMesh.fast_mesh_index(nodes[1]);
-      const DblType densityL = density.get(nodeL, 0);
-      const DblType densityR = density.get(nodeR, 0);
 
-      const DblType viscosityL = viscosity.get(nodeL, 0);
-      const DblType viscosityR = viscosity.get(nodeR, 0);
-      const DblType tviscL = tvisc.get(nodeL, 0);
-      const DblType tviscR = tvisc.get(nodeR, 0);
+      const DblType muIp =
+        0.5 * (viscosity.get(nodeL, 0) + viscosity.get(nodeR, 0));
+      const DblType mutIp = 0.5 * (tvisc.get(nodeL, 0) + tvisc.get(nodeR, 0));
+      const DblType rhoIp =
+        0.5 * (density.get(nodeL, 0) + density.get(nodeR, 0));
+      const DblType tkeIp = 0.5 * (stk::math::max(tke.get(nodeL, 0), 1.0e-12) +
+                                   stk::math::max(tke.get(nodeR, 0), 1.0e-12));
+      const DblType sdrIp = 0.5 * (stk::math::max(sdr.get(nodeL, 0), 1.0e-12) +
+                                   stk::math::max(sdr.get(nodeR, 0), 1.0e-12));
+      const DblType alphaIp = 0.5 * (stk::math::pow(beta.get(nodeL, 0), 1.7) +
+                                     stk::math::pow(beta.get(nodeR, 0), 1.7));
+      const DblType epsilon13Ip =
+        stk::math::pow(betaStar_ * tkeIp * sdrIp, 1.0 / 3.0);
 
-      const DblType diffIp = 0.5 * ((viscosityL + tviscL) / densityL +
-                                    (viscosityR + tviscR) / densityR);
+      const DblType muRansIp = alphaIp * (2.0 - alphaIp) * mutIp;
+
+      // Mij, eigenvectors and eigenvalues
+      DblType Mij[nalu_ngp::NDimMax][nalu_ngp::NDimMax];
+      DblType Q[nalu_ngp::NDimMax][nalu_ngp::NDimMax];
+      DblType D[nalu_ngp::NDimMax][nalu_ngp::NDimMax];
+
+      for (int i = 0; i < ndim; i++)
+        for (int j = 0; j < ndim; j++)
+          Mij[i][j] = 0.5 * (nodalMij.get(nodeL, i * ndim + j) +
+                             nodalMij.get(nodeR, i * ndim + j));
+
+      EigenDecomposition::sym_diagonalize<DblType>(Mij, Q, D);
+
+      // At this point we have Q, the eigenvectors and D the eigenvalues of Mij,
+      // so to create M43, we use Q D^(4/3) Q^T
+      DblType M43[nalu_ngp::NDimMax][nalu_ngp::NDimMax];
+      for (int i = 0; i < ndim; i++)
+        for (int j = 0; j < ndim; j++)
+          M43[i][j] = 0.0;
+
+      const double fourThirds = 4. / 3.;
+      for (int k = 0; k < ndim; k++) {
+        const DblType D43 = stk::math::pow(D[k][k], fourThirds);
+        for (int i = 0; i < ndim; i++) {
+          for (int j = 0; j < ndim; j++) {
+            M43[i][j] += Q[i][k] * Q[j][k] * D43;
+          }
+        }
+      }
+      DblType traceM43 = 0.0;
+      for (int i = 0; i < ndim; i++) {
+        traceM43 += 0.5 * M43[i][i];
+      }
+
+      // Compute CM43
+      DblType CM43 =
+        ams_utils::get_M43_constant<DblType, nalu_ngp::NDimMax>(D, CMdeg_);
+
+      const DblType CM43scale = stk::math::max(
+        stk::math::min(
+          0.5 * (stk::math::pow(avgResAdeq.get(nodeL, 0), 2.0) +
+                 stk::math::pow(avgResAdeq.get(nodeR, 0), 2.0)),
+          30.0),
+        1.0);
+
+      // Do we want arScale in here?
+      const DblType muM43Ip = 0.5 * CM43scale * CM43 * epsilon13Ip * traceM43;
 
       for (int d = 0; d < ndim; ++d) {
         const DblType dxj =
           coordinates.get(nodeR, d) - coordinates.get(nodeL, d);
         asq += av[d] * av[d];
         axdx += av[d] * dxj;
-        udotx += 0.5 * dxj * (vrtm.get(nodeR, d) + vrtm.get(nodeL, d));
+        const DblType uR = vrtm.get(nodeR, d);
+        const DblType uL = vrtm.get(nodeL, d);
+        const DblType avguR = avgu.get(nodeR, d);
+        const DblType avguL = avgu.get(nodeL, d);
+        const DblType fluctuR = (uR - avguR);
+        const DblType fluctuL = (uL - avguL);
+        udotx += 0.5 * dxj * (uR + uL);
+        avgudotx += 0.5 * dxj * (avguR + avguL);
+        fluctudotx += 0.5 * dxj * (fluctuR + fluctuL);
       }
 
-      const DblType pecnum = stk::math::abs(udotx) / (diffIp + eps);
+      const DblType pecnum =
+        rhoIp * stk::math::abs(udotx) / (muIp + eps) +
+        rhoIp * stk::math::abs(avgudotx) / (muRansIp + eps) +
+        rhoIp * stk::math::abs(fluctudotx) / (muM43Ip + eps);
       pecletFactor.get(edge, 0) = pecFunc->execute(pecnum);
     });
 }
