87cc03c6eb0449c3bbdd4cdb0232af5beac1c252
Lawrence Cheung <lcheung@skybridge-login9.sandia.gov>
2021-09-07 11:20:33 -0600

    Force initialize_swept_points every timestep

--- src/actuator/ActuatorBulkDiskFAST.C
+++ src/actuator/ActuatorBulkDiskFAST.C
@@ -52,10 +52,10 @@ ActuatorBulkDiskFAST::initialize_ADM_points(const ActuatorMetaFAST& actMeta)
   // Note: Can change to this conditional below to allow for
   // time-varying turbine yaw/turbine motion (currently assumes fixed
   // yaw)
-  if (!initialized_ADM_points) {
-    initialize_swept_points(actMeta);    
-    initialized_ADM_points = true;
-  }  
+  //if (!initialized_ADM_points) {
+  initialize_swept_points(actMeta);    
+  //initialized_ADM_points = true;
+  //}  
 }
 
 void
