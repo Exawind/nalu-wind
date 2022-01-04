fc9d81d52bf6c480eddc3f775305fd595c11c46c
Robert Wilson <wilsonrv@ornl.gov>
2022-01-04 11:06:42 -0500

    Added initial framework for solution of the One-Eqn Correlation Based Transition Model of Menter 2015 (#921)

--- include/NaluParsedTypes.h
+++ include/NaluParsedTypes.h
@@ -54,6 +54,20 @@ struct SpecDissRate {
   {}
 };
 
+struct GammaInf {
+  double gamma_;
+  GammaInf()
+    : gamma_(1.0)
+  {}
+};
+
+struct GammaOpen {
+  double gamma_;
+  GammaOpen()
+    : gamma_(1.0)
+  {}
+};
+
 struct Temperature {
   double temperature_;
   Temperature()
