Exawind/nalu-wind

commit 616942d40573c089cec844f70cdcc0235590a45d

Fix memory leak (#987)
* Fixed memory leak from NGPApplyCoeff

* Moved NGPApplyCoeff's call to kokkos_free_on_device from destructor to a member function.

NGPApplyCoeff is copy-captured within Kokkos lambdas, so the previous solution
of calling free in the destructor leads to a double-free.

NGPApplyCoeff::free_coeff_applier() must be called before NGPApplyCoeff destruction
to avoid memory leaks.

Co-authored-by: dcdemen <dcdemen@sandia.gov>

include/SolverAlgorithm.h
@@ -39,6 +39,8 @@ struct NGPApplyCoeff
   KOKKOS_DEFAULTED_FUNCTION
   ~NGPApplyCoeff() = default;
 
+  void free_coeff_applier();
+
   KOKKOS_FUNCTION
   void operator()(
     unsigned numMeshobjs,

src/SolverAlgorithm.C
@@ -75,6 +75,13 @@ NGPApplyCoeff::NGPApplyCoeff(EquationSystem* eqSystem)
   }
 }
 
+void NGPApplyCoeff::free_coeff_applier()
+{
+  if(deviceSumInto_ != nullptr) {
+    kokkos_free_on_device(deviceSumInto_);
+  }
+}
+
 void NGPApplyCoeff::extract_diagonal(
   const unsigned int nEntities,
   const stk::mesh::NgpMesh::ConnectedNodes& entities,

include/AssembleEdgeSolverAlgorithm.h
@@ -99,6 +99,7 @@ public:
               smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
           });
       });
+      coeffApplier.free_coeff_applier();
   }
 
 protected:

src/AssembleElemSolverAlgorithm.C
@@ -121,6 +121,7 @@ AssembleElemSolverAlgorithm::execute()
                     smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
       }
     });
+    coeffApplier.free_coeff_applier();
 }
 
 } // namespace nalu

src/AssembleFaceElemSolverAlgorithm.C
@@ -126,6 +126,7 @@ AssembleFaceElemSolverAlgorithm::execute()
                       smdata.scratchIds, smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
         }
     });
+    coeffApplier.free_coeff_applier();
 }
 
 } // namespace nalu

src/AssembleNGPNodeSolverAlgorithm.C
@@ -139,6 +139,7 @@ AssembleNGPNodeSolverAlgorithm::execute()
             smdata.sortPermutation, smdata.rhs, smdata.lhs, __FILE__);
         });
     });
+    coeffApplier.free_coeff_applier();
 }
 
 }  // nalu

commit 5f8afc4c30eda06fc12dc8c4db285868a4f416eb

Fixes to seg fault caused by #987 mem leak fix patch

include/LinearSystem.h
@@ -137,6 +137,7 @@ public:
     return nullptr;
   }
 
+  virtual bool owns_coeff_applier() { return true; }
 
   virtual void sumInto(
     unsigned numEntities,

include/SolverAlgorithm.h
@@ -74,6 +74,7 @@ struct NGPApplyCoeff
   const bool hasOverset_{false};
   const bool extractDiagonal_{false};
   const bool resetOversetRows_{true};
+  const bool linSysOwnsCoeffApplier;
 };
 
 class SolverAlgorithm : public Algorithm

include/TpetraLinearSystem.h
@@ -79,6 +79,8 @@ public:
   CoeffApplier* get_coeff_applier();
   void free_coeff_applier(CoeffApplier* coeffApplier);
 
+  bool owns_coeff_applier() override { return false; }
+
   // Matrix Assembly
   void zeroSystem();
 

src/SolverAlgorithm.C
@@ -63,7 +63,8 @@ NGPApplyCoeff::NGPApplyCoeff(EquationSystem* eqSystem)
     nDim_(eqSystem->linsys_->numDof()),
     hasOverset_(eqSystem->realm_.hasOverset_),
     extractDiagonal_(eqSystem->extractDiagonal_),
-    resetOversetRows_(eqSystem->resetOversetRows_)
+    resetOversetRows_(eqSystem->resetOversetRows_),
+    linSysOwnsCoeffApplier(eqSystem->linsys_->owns_coeff_applier())
 {
   if (extractDiagonal_) {
     diagField_ = nalu_ngp::get_ngp_field(
@@ -77,8 +78,9 @@ NGPApplyCoeff::NGPApplyCoeff(EquationSystem* eqSystem)
 
 void NGPApplyCoeff::free_coeff_applier()
 {
-  if(deviceSumInto_ != nullptr) {
+  if(deviceSumInto_ != nullptr && !linSysOwnsCoeffApplier) {
     kokkos_free_on_device(deviceSumInto_);
+    deviceSumInto_ = nullptr;
   }
 }
 