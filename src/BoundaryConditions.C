25e9835416f726959b08673a1cc7e55646242ec0
Philip Sakievich <psakiev@sandia.gov>
2022-07-13 00:03:05 -0600

    Reformat for readability

--- src/BoundaryConditions.C
+++ src/BoundaryConditions.C
@@ -7,8 +7,6 @@
 // for more details.
 //
 
-
-
 #include <Realm.h>
 #include <BoundaryConditions.h>
 #include <NaluEnv.h>
@@ -17,8 +15,8 @@
 #include <yaml-cpp/yaml.h>
 #include <NaluParsing.h>
 
-namespace sierra{
-namespace nalu{
+namespace sierra {
+namespace nalu {
 
 //==========================================================================
 // Class Definition
@@ -28,7 +26,7 @@ namespace nalu{
 //--------------------------------------------------------------------------
 //-------- constructor -----------------------------------------------------
 //--------------------------------------------------------------------------
-  
+
 //--------------------------------------------------------------------------
 //-------- destructor ------------------------------------------------------
 //--------------------------------------------------------------------------
@@ -37,87 +35,135 @@ namespace nalu{
 //-------- load -----------------------------------------------
 //--------------------------------------------------------------------------
 
-
-template<typename T>
-std::unique_ptr<BoundaryCondition> register_bc(const YAML::Node& node, BoundaryConditions& bcs){
-    std::unique_ptr<BoundaryCondition>this_bc = std::make_unique<T>(bcs);
-    auto* cast_bc = dynamic_cast<T*>(this_bc.get());
-    node >> *cast_bc;
-    return this_bc;
+template <typename T>
+std::unique_ptr<BoundaryCondition>
+register_bc(const YAML::Node& node, BoundaryConditions& bcs)
+{
+  std::unique_ptr<BoundaryCondition> this_bc = std::make_unique<T>(bcs);
+  auto* cast_bc = dynamic_cast<T*>(this_bc.get());
+  node >> *cast_bc;
+  return this_bc;
 }
 
-std::unique_ptr<BoundaryCondition> BoundaryCondition::load(const YAML::Node & node) 
+std::unique_ptr<BoundaryCondition>
+BoundaryCondition::load(const YAML::Node& node)
 {
   std::unique_ptr<BoundaryCondition> this_bc;
-  if ( node["wall_boundary_condition"] ){
-    this_bc = std::move(register_bc<WallBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0()<<"Wall BC name:        " << this_bc->bcName_ \
-                    << " on " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["inflow_boundary_condition"]) {
-    this_bc = std::move(register_bc<InflowBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0()<<"Inflow BC name:        " << this_bc->bcName_ \
-                    << " on " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["open_boundary_condition"]) {
-    this_bc = std::move(register_bc<OpenBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0()<<"Open BC name:        " << this_bc->bcName_ \
-                    << " on " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["symmetry_boundary_condition"]) {
-    this_bc = std::move(register_bc<SymmetryBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0()<<"Symmetry BC name:        " << this_bc->bcName_ \
-                    << " on " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["abltop_boundary_condition"]) {
-    this_bc = std::move(register_bc<ABLTopBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0()<<"ABLTop BC name:        " << this_bc->bcName_ \
-                    << " on " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["periodic_boundary_condition"]) {
-    this_bc = std::move(register_bc<PeriodicBoundaryConditionData>(node, boundaryConditions_));
-    auto* periodicBC = dynamic_cast<PeriodicBoundaryConditionData*>(this_bc.get());
-    NaluEnv::self().naluOutputP0() << "Periodic BC name:    " << periodicBC->bcName_
-                    << " between " << periodicBC->masterSlave_.master_
-                    << " and "<< periodicBC->masterSlave_.slave_ << std::endl;
-  }
-  else if (node["non_conformal_boundary_condition"]) {
-    this_bc = std::move(register_bc<NonConformalBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0() << "NonConformal BC name:    " << this_bc->bcName_
-                    << " using " << this_bc->targetName_ << std::endl;
-  }
-  else if (node["overset_boundary_condition"]) {
-    this_bc = std::move(register_bc<OversetBoundaryConditionData>(node, boundaryConditions_));
-    NaluEnv::self().naluOutputP0() << "Overset BC name: " << this_bc->bcName_ << std::endl;
-  } 
-  else {
-    throw std::runtime_error("parser error BoundaryConditions::load: no such bc type");
+  if (node["wall_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<WallBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "Wall BC name:        " << this_bc->bcName_ << " on "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["inflow_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<InflowBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "Inflow BC name:        " << this_bc->bcName_ << " on "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["open_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<OpenBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "Open BC name:        " << this_bc->bcName_ << " on "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["symmetry_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<SymmetryBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "Symmetry BC name:        " << this_bc->bcName_ << " on "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["abltop_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<ABLTopBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "ABLTop BC name:        " << this_bc->bcName_ << " on "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["periodic_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<PeriodicBoundaryConditionData>(node, boundaryConditions_));
+
+    auto* periodicBC =
+      dynamic_cast<PeriodicBoundaryConditionData*>(this_bc.get());
+
+    NaluEnv::self().naluOutputP0()
+      << "Periodic BC name:    " << periodicBC->bcName_ << " between "
+      << periodicBC->masterSlave_.master_ << " and "
+      << periodicBC->masterSlave_.slave_ << std::endl;
+
+  } else if (node["non_conformal_boundary_condition"]) {
+    this_bc = std::move(register_bc<NonConformalBoundaryConditionData>(
+      node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "NonConformal BC name:    " << this_bc->bcName_ << " using "
+      << this_bc->targetName_ << std::endl;
+
+  } else if (node["overset_boundary_condition"]) {
+    this_bc = std::move(
+      register_bc<OversetBoundaryConditionData>(node, boundaryConditions_));
+
+    NaluEnv::self().naluOutputP0()
+      << "Overset BC name: " << this_bc->bcName_ << std::endl;
+
+  } else {
+    throw std::runtime_error(
+      "parser error BoundaryConditions::load: no such bc type");
   }
   return this_bc;
 }
 
-  Simulation* BoundaryCondition::root() { return parent()->root(); }
-  BoundaryConditions *BoundaryCondition::parent() { return &boundaryConditions_; }
+Simulation*
+BoundaryCondition::root()
+{
+  return parent()->root();
+}
+BoundaryConditions*
+BoundaryCondition::parent()
+{
+  return &boundaryConditions_;
+}
 
-  Simulation* BoundaryConditions::root() { return parent()->root(); }
-  Realm *BoundaryConditions::parent() { return &realm_; }
+Simulation*
+BoundaryConditions::root()
+{
+  return parent()->root();
+}
+Realm*
+BoundaryConditions::parent()
+{
+  return &realm_;
+}
 
-void BoundaryConditions::load(const YAML::Node &node)
+void
+BoundaryConditions::load(const YAML::Node& node)
 {
   BoundaryCondition bc_factory(*this);
 
-  if(node["boundary_conditions"]) {
+  if (node["boundary_conditions"]) {
     const YAML::Node boundary_conditions = node["boundary_conditions"];
-    for ( size_t iboundary_condition = 0; iboundary_condition < boundary_conditions.size(); ++iboundary_condition ) {
-      const YAML::Node boundary_condition_node = boundary_conditions[iboundary_condition];
-      boundaryConditionVector_.emplace_back(bc_factory.load(boundary_condition_node));
+    for (size_t iboundary_condition = 0;
+         iboundary_condition < boundary_conditions.size();
+         ++iboundary_condition) {
+      const YAML::Node boundary_condition_node =
+        boundary_conditions[iboundary_condition];
+      boundaryConditionVector_.emplace_back(
+        bc_factory.load(boundary_condition_node));
     }
-  }
-  else {
+  } else {
     throw std::runtime_error("parser error BoundaryConditions::load");
   }
 }
 
-
 } // namespace nalu
-} // namespace Sierra
+} // namespace sierra
