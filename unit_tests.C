30ed9d49ba012918645ae63ad8d97cf92b39d020
psakievich <psakiev@sandia.gov>
2022-08-18 12:24:25 -0600

    A few files missed by the global clang-format (#1015)

--- unit_tests.C
+++ unit_tests.C
@@ -7,9 +7,8 @@
 // for more details.
 //
 
-
-#include "gtest/gtest.h"                // for InitGoogleTest, etc
-#include "mpi.h"                        // for MPI_Comm_rank, MPI_Finalize, etc
+#include "gtest/gtest.h" // for InitGoogleTest, etc
+#include "mpi.h"         // for MPI_Comm_rank, MPI_Finalize, etc
 #include "Kokkos_Core.hpp"
 #include "stk_util/parallel/Parallel.hpp"
 
@@ -17,45 +16,45 @@
 #include "NaluEnv.h"
 #include "master_element/MasterElementFactory.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char** argv)
 {
-    MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
-    sierra::nalu::NaluEnv::self();
-    Kokkos::initialize(argc, argv);
-    int returnVal = 0;
+  sierra::nalu::NaluEnv::self();
+  Kokkos::initialize(argc, argv);
+  int returnVal = 0;
 
 #ifdef KOKKOS_ENABLE_CUDA
-    const size_t nalu_stack_size=16384;
-    cudaDeviceSetLimit (cudaLimitStackSize, nalu_stack_size);
+  const size_t nalu_stack_size = 16384;
+  cudaDeviceSetLimit(cudaLimitStackSize, nalu_stack_size);
 #endif
-    // Create a dummy nested scope to ensure destructors are called before
-    // Kokkos::finalize_all. The instances owning threaded Kokkos loops must be
-    // cleared out before Kokkos::finalize is called.
-    {
-      // clang-format off
+  // Create a dummy nested scope to ensure destructors are called before
+  // Kokkos::finalize_all. The instances owning threaded Kokkos loops must be
+  // cleared out before Kokkos::finalize is called.
+  {
+    // clang-format off
       namespace version = sierra::nalu::version;
       sierra::nalu::NaluEnv::self().naluOutputP0()
         << "   Nalu-Wind Version: " << version::NaluVersionTag << std::endl
         << "   Nalu-Wind GIT Commit SHA: " << version::NaluGitCommitSHA
         << ((version::RepoIsDirty == "DIRTY") ? ("-" + version::RepoIsDirty) : "") << std::endl
         << "   Trilinos Version: " << version::TrilinosVersionTag << std::endl << std::endl;
-      // clang-format on
+    // clang-format on
 
-      testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
 
-      returnVal = RUN_ALL_TESTS();
+    returnVal = RUN_ALL_TESTS();
 
-      // Force deallocation of all MasterElements created. This is necessary
-      // when specific unit tests are run using the gtest_filter option that
-      // provides no mechanism for call the destructors of the master elements
-      // created for those tests.
-      sierra::nalu::MasterElementRepo::clear();
-    }
+    // Force deallocation of all MasterElements created. This is necessary
+    // when specific unit tests are run using the gtest_filter option that
+    // provides no mechanism for call the destructors of the master elements
+    // created for those tests.
+    sierra::nalu::MasterElementRepo::clear();
+  }
 
-    Kokkos::finalize_all();
-    MPI_Finalize();
+  Kokkos::finalize_all();
+  MPI_Finalize();
 
-    return returnVal;
+  return returnVal;
 }
-
