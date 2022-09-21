2bf93a7537d0d5f721d023fdcb1a825ab46607b7
Philip Sakievich <psakiev@sandia.gov>
2022-09-21 15:51:49 -0600

    Fix typo and add missing copyright

--- unit_tests/UnitTestVSpace.C
+++ unit_tests/UnitTestVSpace.C
@@ -6,7 +6,7 @@
 // This software is released under the BSD 3-clause license. See LICENSE file
 // for more details.
 //
-// Original implementation of this code by Shreyas Anthan for AMR-Wind
+// Original implementation of this code by Shreyas Ananthan for AMR-Wind
 // - (https://github.com/Exawind/amr-wind)
 //
 // Adapted to use Kokkos
