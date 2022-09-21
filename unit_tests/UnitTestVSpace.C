c5e2c2c8c870a734fc03223537c9ea00cb2b4102
Philip Sakievich <psakiev@sandia.gov>
2022-09-21 15:47:54 -0600

    Add license statement and credit to original source

--- unit_tests/UnitTestVSpace.C
+++ unit_tests/UnitTestVSpace.C
@@ -1,3 +1,16 @@
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+// Original implementation of this code by Shreyas Anthan for AMR-Wind
+// - (https://github.com/Exawind/amr-wind)
+//
+// Adapted to use Kokkos
+
 #include "gtest/gtest.h"
 #include "vs/vector_space.h"
 #include "vs/trig_ops.h"
