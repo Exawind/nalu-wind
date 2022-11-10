9005b3a045c138c3e1774b2ccd2c8c5a0cb0d018
psakiev <psakiev@sandia.gov>
2022-11-10 13:45:43 -0700

    Fix typos from search and replace

--- unit_tests/UnitTestFieldManager.C
+++ unit_tests/UnitTestFieldManager.C
@@ -26,6 +26,7 @@ protected:
     meta_ = builder.create_meta_data();
     key_ = "velocity";
   }
+  stk::mesh::MetaData& meta() { return *(meta_.get()); }
   std::shared_ptr<stk::mesh::MetaData> meta_;
   std::string key_;
 };
@@ -34,14 +35,14 @@ TEST_F(FieldManagerTest, nameIsEnoughInfoToRegisterAField)
 {
   const int num_states = 2;
   std::string name = "velocity";
-  FieldManager fm(meta_->num_states);
+  FieldManager fm(meta(), num_states);
   EXPECT_FALSE(fm.field_exists(name));
 
-  auto ptr = fm.register_field(name, meta_->get_parts());
+  auto ptr = fm.register_field(name, meta().get_parts());
 
   // check that field is on the mesh
   const auto findFieldPtr =
-    meta_->get_field<VectorFieldType>(stk::topology::NODE_RANK, name);
+    meta().get_field<VectorFieldType>(stk::topology::NODE_RANK, name);
   EXPECT_EQ(findFieldPtr, std::get<VectorFieldType*>(ptr));
   EXPECT_TRUE(fm.field_exists(name));
 
@@ -52,7 +53,7 @@ TEST_F(FieldManagerTest, nameIsEnoughInfoToRegisterAField)
 TEST_F(FieldManagerTest, throwsForFieldNotInDatabase)
 {
   const int num_states = 2;
-  FieldManager f(meta_->num_states);
+  FieldManager f(meta(), num_states);
   EXPECT_THROW(f.field_exists("acrazyqoi"), std::runtime_error);
 }
 
@@ -61,11 +62,11 @@ TEST_F(FieldManagerTest, canRegisterDifferentFieldTypesThroughOneInterface)
   const std::string vectorName = "velocity";
   const std::string scalarName = "temperature";
   const int num_states = 2;
-  FieldManager f(meta_->num_states);
+  FieldManager f(meta(), num_states);
   EXPECT_FALSE(f.field_exists(vectorName));
   EXPECT_FALSE(f.field_exists(scalarName));
-  EXPECT_NO_THROW(f.register_field(vectorName, meta_->get_parts()));
-  EXPECT_NO_THROW(f.register_field(scalarName, meta_->universal_part()));
+  EXPECT_NO_THROW(f.register_field(vectorName, meta().get_parts()));
+  EXPECT_NO_THROW(f.register_field(scalarName, meta().universal_part()));
   EXPECT_TRUE(f.field_exists(vectorName));
   EXPECT_TRUE(f.field_exists(scalarName));
 }
@@ -74,10 +75,10 @@ TEST_F(FieldManagerTest, fieldCanBeRegisteredMultipleTimes)
 {
   const std::string name = "velocity";
   const int num_states = 3;
-  FieldManager fm(meta_->num_states);
+  FieldManager fm(meta(), num_states);
   EXPECT_FALSE(fm.field_exists(name));
-  EXPECT_NO_THROW(fm.register_field(name, meta_->get_parts()));
-  EXPECT_NO_THROW(fm.register_field(name, meta_->universal_part()));
+  EXPECT_NO_THROW(fm.register_field(name, meta().get_parts()));
+  EXPECT_NO_THROW(fm.register_field(name, meta().universal_part()));
   EXPECT_TRUE(fm.field_exists(name));
 }
 
@@ -85,9 +86,9 @@ TEST_F(FieldManagerTest, undefinedFieldCantBeRegistered)
 {
   const std::string name = "fields_of_gold";
   const int num_states = 3;
-  FieldManager fm(meta_->num_states);
+  FieldManager fm(meta(), num_states);
   EXPECT_THROW(
-    fm.register_field(name, meta_->universal_part()), std::runtime_error);
+    fm.register_field(name, meta().universal_part()), std::runtime_error);
 }
 } // namespace
 } // namespace nalu
