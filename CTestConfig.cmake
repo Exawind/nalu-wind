042f2e3f9c112876753fe5c878407c15675fdbc2
psakievich <psakiev@sandia.gov>
2022-11-17 20:44:15 -0700

    Change CDash to Common Dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,5 +11,5 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Nalu-Wind")
+set(CTEST_DROP_LOCATION "/submit.php?project=Exawind")
 set(CTEST_DROP_SITE_CDASH TRUE)
