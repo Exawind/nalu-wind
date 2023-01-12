a5d0e6cd76ed4b3230a446284d1462efa0d3c190
psakievich <psakiev@sandia.gov>
2023-01-12 08:15:39 -0700

    Fix some RangePolicies (#1103)
    * Fix some RangePolicies
    
    #1102 got a few RangePolicies wrong in the OpenFAST executors.  All actuator code currently executes on host.
    
    * Update ActuatorBulkDiskFAST.C
    
    * Update ActuatorExecutorsFASTNgp.C
    
    * Format

--- src/aero/actuator/ActuatorBulkFAST.C
+++ src/aero/actuator/ActuatorBulkFAST.C
@@ -300,14 +300,14 @@ void
 ActuatorBulkFAST::output_torque_info(stk::mesh::BulkData& stkBulk)
 {
   Kokkos::parallel_for(
-    "setUpTorqueCalc", DeviceRangePolicy(0, hubLocations_.extent(0)),
+    "setUpTorqueCalc", HostRangePolicy(0, hubLocations_.extent(0)),
     ActFastSetUpThrustCalc(*this));
 
   actuator_utils::reduce_view_on_host(hubLocations_);
   actuator_utils::reduce_view_on_host(hubOrientation_);
 
   Kokkos::parallel_for(
-    "computeTorque", DeviceRangePolicy(0, coarseSearchElemIds_.extent(0)),
+    "computeTorque", HostRangePolicy(0, coarseSearchElemIds_.extent(0)),
     ActFastComputeThrust(*this, stkBulk));
   actuator_utils::reduce_view_on_host(turbineThrust_);
   actuator_utils::reduce_view_on_host(turbineTorque_);
