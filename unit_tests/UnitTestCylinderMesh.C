1e939f4a31d84e66c4b5ca517d411ed6d4bc630e
Alan Williams <william@sandia.gov>
2023-02-13 15:17:47 -0700

    forgot to clang-format again...

--- unit_tests/UnitTestCylinderMesh.C
+++ unit_tests/UnitTestCylinderMesh.C
@@ -28,12 +28,15 @@ test_cylinder_mesh_field_values(
     bulk.get_buckets(stk::topology::NODE_RANK, all_local);
 
   stk::mesh::NgpMesh ngpMesh(bulk);
-  stk::mesh::NgpField<double>& ngpCoordField = stk::mesh::get_updated_ngp_field<double>(*coordField);
-  stk::mesh::NgpField<double>& ngpTestField = stk::mesh::get_updated_ngp_field<double>(*testField);
+  stk::mesh::NgpField<double>& ngpCoordField =
+    stk::mesh::get_updated_ngp_field<double>(*coordField);
+  stk::mesh::NgpField<double>& ngpTestField =
+    stk::mesh::get_updated_ngp_field<double>(*testField);
 
   const int bytes_per_team = 0;
   const int bytes_per_thread = 0;
-  auto team_exec = sierra::nalu::get_device_team_policy(nodeBuckets.size(), bytes_per_team, bytes_per_thread);
+  auto team_exec = sierra::nalu::get_device_team_policy(
+    nodeBuckets.size(), bytes_per_team, bytes_per_thread);
 
   const double xDelta = 0.1;
   const double yDelta = 0.2;
@@ -49,14 +52,13 @@ test_cylinder_mesh_field_values(
       const size_t bucketLen = bkt.size();
 
       Kokkos::parallel_for(
-        Kokkos::TeamThreadRange(team, bucketLen),
-        [&](const size_t& bktIndex) {
-            stk::mesh::Entity node = bkt[bktIndex];
-            stk::mesh::FastMeshIndex nodeIndex = ngpMesh.fast_mesh_index(node);
+        Kokkos::TeamThreadRange(team, bucketLen), [&](const size_t& bktIndex) {
+          stk::mesh::Entity node = bkt[bktIndex];
+          stk::mesh::FastMeshIndex nodeIndex = ngpMesh.fast_mesh_index(node);
 
-            ngpTestField(nodeIndex,0) = ngpCoordField(nodeIndex,0) + xDelta;
-            ngpTestField(nodeIndex,1) = ngpCoordField(nodeIndex,1) + yDelta;
-            ngpTestField(nodeIndex,2) = ngpCoordField(nodeIndex,2) + zDelta;
+          ngpTestField(nodeIndex, 0) = ngpCoordField(nodeIndex, 0) + xDelta;
+          ngpTestField(nodeIndex, 1) = ngpCoordField(nodeIndex, 1) + yDelta;
+          ngpTestField(nodeIndex, 2) = ngpCoordField(nodeIndex, 2) + zDelta;
         });
     });
 
@@ -66,11 +68,13 @@ test_cylinder_mesh_field_values(
   const double tol = 1.0e-12;
   for (const stk::mesh::Bucket* bkt : nodeBuckets) {
     for (stk::mesh::Entity node : *bkt) {
-      const double* coordFieldData = reinterpret_cast<const double*>(stk::mesh::field_data(*coordField, node));
-      const double* testFieldData = reinterpret_cast<const double*>(stk::mesh::field_data(*testField, node));
-      EXPECT_NEAR(testFieldData[0], (coordFieldData[0]+xDelta), tol);
-      EXPECT_NEAR(testFieldData[1], (coordFieldData[1]+yDelta), tol);
-      EXPECT_NEAR(testFieldData[2], (coordFieldData[2]+zDelta), tol);
+      const double* coordFieldData = reinterpret_cast<const double*>(
+        stk::mesh::field_data(*coordField, node));
+      const double* testFieldData = reinterpret_cast<const double*>(
+        stk::mesh::field_data(*testField, node));
+      EXPECT_NEAR(testFieldData[0], (coordFieldData[0] + xDelta), tol);
+      EXPECT_NEAR(testFieldData[1], (coordFieldData[1] + yDelta), tol);
+      EXPECT_NEAR(testFieldData[2], (coordFieldData[2] + zDelta), tol);
     }
   }
 }
@@ -88,4 +92,3 @@ TEST_F(CylinderMesh, basic_setup)
     unit_test_utils::dump_mesh(*bulk, {});
   }
 }
-
