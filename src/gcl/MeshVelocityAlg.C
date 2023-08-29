2b156985338eddbcc3d5dedfbea39b54b46f4925
psakievich <psakiev@sandia.gov>
2023-08-29 03:01:42 -0600

    format

--- src/gcl/MeshVelocityAlg.C
+++ src/gcl/MeshVelocityAlg.C
@@ -79,7 +79,7 @@ MeshVelocityAlg<AlgTraits>::MeshVelocityAlg(Realm& realm, stk::mesh::Part* part)
     NUM_IP, isoParCoords_, isoCoordsShapeFcnHostView_.data());
   Kokkos::deep_copy(isoCoordsShapeFcnDeviceView_, isoCoordsShapeFcnHostView_);
 
-  Kokkos::View<int[12][4], Kokkos::HostSpace>scsFaceNodeMapHostView;
+  Kokkos::View<int[12][4], Kokkos::HostSpace> scsFaceNodeMapHostView;
   for (int i = 0; i < 12; ++i) {
     for (int j = 0; j < 4; ++j) {
       scsFaceNodeMapHostView(i, j) = scsFaceNodeMap_[i][j];
