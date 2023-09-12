35f57cda0e084b73cf66d663523456f5bb7d8a6b
psakiev <psakiev@sandia.gov>
2023-09-12 05:18:20 -0600

    Add prototype for smartptr

---
+++ unit_tests/UnitTestSmartFieldPtr.C
@@ -0,0 +1,44 @@
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+
+#include <gtest/gtest.h>
+#include "UnitTestUtils.h"
+#include <stk_mesh/base/GetNgpField.hpp>
+
+template <typename T>
+class SmartFieldPtr{
+public:
+  SmartFieldPtr(stk::mesh::NgpField<T>& fieldRef):fieldPtr_(&fieldRef){}
+  SmartFieldPtr(const SmartFieldPtr& src): is_a_copy_(true), fieldPtr_(src.fieldPtr_){
+    fieldPtr_->sync_to_device();
+  }
+  ~SmartFieldPtr(){
+    if(is_a_copy_){
+      fieldPtr_->modify_on_device();
+    }
+  }
+protected:
+  bool is_a_copy_{false};
+  stk::mesh::NgpField<T>* fieldPtr_;
+};
+
+TEST_F(Hex8Mesh, SmartFieldPtr){
+  fill_mesh_and_initialize_test_fields();
+  auto* field = fieldManager->get_field_ptr<ScalarFieldType>("scalarQ");
+  stk::mesh::NgpField<ScalarFieldType>& ngpField =
+    stk::mesh::get_updated_ngp_field<ScalarFieldType>(*field);
+  ngpField.modify_on_host();
+  ASSERT_TRUE(ngpField.need_sync_to_device());
+  auto sPtr = SmartFieldPtr(ngpField);
+  Kokkos::parallel_for(1, KOKKOS_LAMBDA(int){
+                       (void)sPtr;});
+  EXPECT_FALSE(ngpField.need_sync_to_device());
+  EXPECT_TRUE(ngpField.need_sync_to_host());
+
+}
