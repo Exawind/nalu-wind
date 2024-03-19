56c0e6dbca1255eba0e2f077bc59a1b4e286669c
psakievich <psakiev@sandia.gov>
2024-03-19 08:58:36 -0600

    Update Realm.C

--- src/Realm.C
+++ src/Realm.C
@@ -3423,10 +3423,10 @@ Realm::populate_restart(double& timeStepNm1, int& timeStepCount)
         auto* field = stk::mesh::get_field_by_name(fname, meta_data());
         if (field == nullptr)
           continue;
-        fld->clear_sync_state();
         const unsigned numStates = field->number_of_states();
         for (unsigned i = 0; i < numStates; ++i) {
           auto* fld = field->field_state(static_cast<stk::mesh::FieldState>(i));
+          fld->clear_sync_state();
           fld->modify_on_host();
           ngp_field_manager().get_field<double>(fld->mesh_meta_data_ordinal());
           fld->sync_to_device();
