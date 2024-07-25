4a527811b72a56f9be02f01035026a04f3a0149f
Michael Kuhn <mkuhn@nrel.gov>
2024-07-25 16:38:05 -0600

    create basic buoyancy term as node kernel, named buoyancy_density

---
+++ src/node_kernels/MomentumBuoyancyNodeKernel.C
@@ -0,0 +1,64 @@
+// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
+// Northwest Research Associates. Under the terms of Contract DE-NA0003525
+// with NTESS, the U.S. Government retains certain rights in this software.
+//
+// This software is released under the BSD 3-clause license. See LICENSE file
+// for more details.
+//
+
+#include "node_kernels/MomentumBuoyancyNodeKernel.h"
+#include "Realm.h"
+
+#include "stk_mesh/base/MetaData.hpp"
+#include "stk_mesh/base/Types.hpp"
+#include "utils/StkHelpers.h"
+
+#include "SolutionOptions.h"
+
+namespace sierra {
+namespace nalu {
+
+MomentumBuoyancyNodeKernel::MomentumBuoyancyNodeKernel(
+  const stk::mesh::BulkData& bulk, const SolutionOptions& solnOpts)
+  : NGPNodeKernel<MomentumBuoyancyNodeKernel>(),
+    nDim_(bulk.mesh_meta_data().spatial_dimension()),
+    rhoRef_(solnOpts.referenceDensity_)
+{
+  const auto& meta = bulk.mesh_meta_data();
+
+  dualNodalVolumeID_ = get_field_ordinal(meta, "dual_nodal_volume");
+  densityNp1ID_ = get_field_ordinal(meta, "density");
+
+  const std::vector<double>& solnOptsGravity =
+    solnOpts.get_gravity_vector(nDim_);
+  for (int i = 0; i < nDim_; i++)
+    gravity_[i] = solnOptsGravity[i];
+}
+
+void
+MomentumBuoyancyNodeKernel::setup(Realm& realm)
+{
+  const auto& fieldMgr = realm.ngp_field_manager();
+  dualNodalVolume_ = fieldMgr.get_field<double>(dualNodalVolumeID_);
+  densityNp1_ = fieldMgr.get_field<double>(densityNp1ID_);
+}
+
+KOKKOS_FUNCTION
+void
+MomentumBuoyancyNodeKernel::execute(
+  NodeKernelTraits::LhsType&,
+  NodeKernelTraits::RhsType& rhs,
+  const stk::mesh::FastMeshIndex& node)
+{
+  const NodeKernelTraits::DblType rhoNp1 = densityNp1_.get(node, 0);
+  const NodeKernelTraits::DblType dualVolume = dualNodalVolume_.get(node, 0);
+  const double fac = (rhoNp1 - rhoRef_) * dualVolume;
+
+  for (int i = 0; i < nDim_; ++i) {
+    rhs(i) += fac * gravity_[i];
+  }
+}
+
+} // namespace nalu
+} // namespace sierra
