c58c9163c1f283d9f2e4193fba5debd654006fef
BumseokLee <bumseok.lee@nrel.gov>
2025-02-12 13:35:57 -0700

    make trasition=true for IDDES

--- src/SolutionOptions.C
+++ src/SolutionOptions.C
@@ -254,7 +254,7 @@ SolutionOptions::load(const YAML::Node& y_node)
         y_solution_options, "strelets_upwinding", useStreletsUpwinding_,
         useStreletsUpwinding_);
     }
-    if (turbulenceModel_ == TurbulenceModel::SST) {
+    if (turbulenceModel_ == TurbulenceModel::SST || turbulenceModel_ == TurbulenceModel::SST_IDDES ) {
       get_if_present(
         y_solution_options, "transition_model", transition_model_,
         transition_model_);
