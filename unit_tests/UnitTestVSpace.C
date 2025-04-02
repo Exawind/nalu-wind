12c08ca7c30a1e23d7cbd1bdc3f62035962c5d48
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-04-02 16:17:24 -0600

    Try modifying vector for kernel

--- unit_tests/UnitTestVSpace.C
+++ unit_tests/UnitTestVSpace.C
@@ -193,7 +193,11 @@ test_tensor_skewsym()
       vs::Vector v(1.0,2.0,3.0);
       vs::Vector u(4.0,5.0,6.0);
       
-      dv = (vs::skew_sym(v) & u) - (v ^ u);
+      auto crossdiff = (vs::skew_sym(v) & u) - (v ^ u);
+
+      dv[0] = crossdiff[0];
+      dv[1] = crossdiff[1];
+      dv[2] = crossdiff[2];
 
     });
   dvec.modify<sierra::nalu::MemSpace>();
