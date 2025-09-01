5e2a4715d38a34b6d98ade5363396209bb83fccd
Timothy A. Smith <2timothy18@gmail.com>
2025-09-01 06:28:15 -0600

    Clean up SIMD instruction test logic

--- unit_tests/UnitTestSimdBasic.C
+++ unit_tests/UnitTestSimdBasic.C
@@ -18,16 +18,8 @@ TEST(Simd, basic)
 
 TEST(Simd, whichInstructions)
 {
-#if defined(STK_SIMD_AVX512)
-  std::cout << "STK_SIMD_AVX512";
-#elif defined(STK_SIMD_AVX)
-  std::cout << "STK_SIMD_AVX";
-#elif defined(STK_SIMD_SSE)
-  std::cout << "STK_SIMD_SSE";
-#else
-  std::cout << "no simd instructions!" << std::endl;
-#endif
-  std::cout << ", stk::simd::ndoubles=" << stk::simd::ndoubles << std::endl;
+  std::cout << "STK simd native type: " << typeid(kokkos_simd::simd_abi::native).name() << std::endl;
+  std::cout << "stk::simd::ndoubles = " << stk::simd::ndoubles << std::endl;
 }
 
 typedef std::vector<double, non_std::AlignedAllocator<double, 64>>
