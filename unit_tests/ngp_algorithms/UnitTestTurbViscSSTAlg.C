3d2265de4df7185cb6a1f9f474f6ccb751a666b9
Jon Rood <jon.rood@nrel.gov>
2026-06-18 14:53:05 -0600

    Fix class in NGP_turb_visc_sstlr_alg unit test.

--- unit_tests/ngp_algorithms/UnitTestTurbViscSSTAlg.C
+++ unit_tests/ngp_algorithms/UnitTestTurbViscSSTAlg.C
@@ -76,7 +76,7 @@ TEST_F(SSTKernelHex8Mesh, NGP_turb_visc_sstlr_alg)
   unit_test_utils::HelperObjects helperObjs(
     bulk_, stk::topology::HEX_8, 1, partVec_[0]);
 
-  sierra::kynema_ugf::TurbViscSSTAlg TurbViscSSTLRAlg(
+  sierra::kynema_ugf::TurbViscSSTLRAlg TurbViscSSTLRAlg(
     helperObjs.realm, partVec_[0], tvisc_);
 
   TurbViscSSTLRAlg.execute();
