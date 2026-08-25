394214df2cdcd04b10ed2f269ddf6884dd859f8a
Ganesh Vijayakumar <ganesh.vijayakumar@nlr.gov>
2026-08-25 12:50:57 -0600

    Remove copyright that was copied over

--- src/aero/FMBContainer.C
+++ src/aero/FMBContainer.C
@@ -1,11 +1,3 @@
-// Copyright 2017 National Technology & Engineering Solutions of Sandia, LLC
-// (NTESS), National Renewable Energy Laboratory, University of Texas Austin,
-// Northwest Research Associates. Under the terms of Contract DE-NA0003525
-// with NTESS, the U.S. Government retains certain rights in this software.
-//
-// This software is released under the BSD 3-clause license. See LICENSE file
-// for more details.
-//
 // This file is only compiled when ENABLE_KYNEMA_FMB_SIXDOF is on.
 #include <aero/FMBContainer.h>
 #include <KynemaUGFEnv.h>
